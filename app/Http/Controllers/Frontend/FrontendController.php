601087eb82ebf94ce6076e2aeff53afe688096e6
khaleel raweh <khaleelvisa@gmail.com>
2024-03-04 21:07:51 +0300

    worldSeeder.php

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -55,11 +55,50 @@ class FrontendController extends Controller
 
     public function courses($slug = null)
     {
+        $courses = Course::with('photos', 'firstMedia', 'courseCategory');
         if ($slug == null) {
-            $courses = Course::with('firstMedia', 'lastMedia', 'courseCategory')->inRandomOrder()->Active()->ActiveCourseCategory()
-                ->paginate($this->paginationLimit);
+            // $courses = $courses->inRandomOrder()->Active()->ActiveCourseCategory()
+            //     ->paginate($this->paginationLimit);
+            $courses = $courses->ActiveCourseCategory();
         } else {
+
+            // get product category where its slug is equail to came slug 
+            $course_category = CourseCategory::whereSlug($slug)
+                ->whereStatus(true)
+                ->first();
+
+            //check product category if its parent_id is null means its one of the main category
+            if (is_null($course_category->parent_id)) {
+
+                // get all sub categories id where there parentId is equals to $product_category->parent_id than mentioned in the check which are main category
+                $categoriesIds = ProductCategory::whereParentId($course_category->id)
+                    ->whereStatus(true)
+                    ->pluck('id')
+                    ->toArray();
+
+                // get all products from all sub categories by using whereHas('category',...)
+                $courses = $courses->whereHas('courseCategory', function ($query) use ($categoriesIds) {
+                    $query->whereIn('id', $categoriesIds);
+                });
+            } else {
+
+                //else if product category is submenu 
+
+                //get all products related to this sub category where subcategory's slug equal to came slug and its status is true :means active
+                $courses = $courses->with('courseCategory')->whereHas('courseCategory', function ($query) use ($slug) {
+                    $query->where([
+                        'slug' => $slug,
+                        'status'   => true
+                    ]);
+                });
+            }
         }
+
+        //check product if is active and has quantity and order it by switch case choise
+        $courses = $courses->active()
+            ->inRandomOrder()
+            ->paginate($this->paginationLimit);
+
 
         return view('frontend.course-list', compact('courses'));
     }

60e46cedaaf1b2cb008374192162b3aca4dd32bd
khaleel raweh <khaleelvisa@gmail.com>
2024-03-14 03:19:09 +0300

    all is ok

--- database/seeders/EntrustSeeder.php
+++ database/seeders/EntrustSeeder.php
@@ -58,6 +58,15 @@ class EntrustSeeder extends Seeder
         $customerRole->save();
 
 
+        //LecturerRole
+        $lecturerRole = new Role();
+        $lecturerRole->name         = 'lecturer';
+        $lecturerRole->display_name = 'course lecturer'; // optional
+        $lecturerRole->description  = 'lecturer is the person who  lecture courses'; // optional
+        $lecturerRole->allowed_route = null;
+        $lecturerRole->save();
+
+
         //------------- 02- Users  ------------//
         // Create Admin
         $admin = new User();
@@ -101,10 +110,25 @@ class EntrustSeeder extends Seeder
             'remember_token' => Str::random(10),
         ]);
 
+        // Create lecturer
+        $lecturer = User::create([
+            'first_name' => 'lecturer',
+            'last_name' => 'person',
+            'username' => 'lecturer',
+            'email' => 'lecturer@gmail.com',
+            'email_verified_at' => now(),
+            'mobile' => '00967772036134',
+            'password' => bcrypt('123123123'),
+            'user_image' => 'avator.svg',
+            'status' => 1,
+            'remember_token' => Str::random(10),
+        ]);
+
         //------------- 03- AttachRoles To  Users  ------------//
         $admin->attachRole($adminRole);
         $supervisor->attachRole($supervisorRole);
         $customer->attachRole($customerRole);
+        $lecturer->attachRole($lecturerRole);
 
 
         //------------- 04-  Create random customer and  AttachRole to customerRole  ------------//
@@ -125,6 +149,27 @@ class EntrustSeeder extends Seeder
 
             //Add customerRole to RandomCusomer
             $random_customer->attachRole($customerRole);
+        } //end for
+
+
+        //------------- 04-2-  Create random lecturer and  AttachRole to lecturerRole  ------------//
+        for ($i = 1; $i <= 20; $i++) {
+            //Create random lecturer
+            $random_lecturer = User::create([
+                'first_name' => $faker->firstName,
+                'last_name' => $faker->lastName,
+                'username' => $faker->unique()->userName,
+                'email' => $faker->unique()->email,
+                'email_verified_at' => now(),
+                'mobile' => '0096777' . $faker->numberBetween(1000000, 9999999),
+                'password' => bcrypt('123123123'),
+                'user_image' => 'avator.svg',
+                'status' => 1,
+                'remember_token' => Str::random(10),
+            ]);
+
+            //Add lecturerRole to Randomlecturer
+            $random_lecturer->attachRole($lecturerRole);
         } //end for
 
 

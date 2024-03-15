926816595d7221253fae1925cfcd8f81066ea095
khaleel raweh <khaleelvisa@gmail.com>
2024-03-15 05:48:55 +0300

    specializationseeder is clean

--- database/seeders/SpecializationSeeder.php
+++ database/seeders/SpecializationSeeder.php
@@ -16,21 +16,9 @@ class SpecializationSeeder extends Seeder
      */
     public function run()
     {
-        // Get active users
-        // $users = User::whereHas('roles', function ($query) {
-        //     $query->where('name', 'lecturer');
-        // })->pluck('id');
-
 
         $Desiner = Specialization::create(['name' => ['ar' => 'مصمم', 'en'    =>  'Desiner']]);
-        // $Desiner->users()->sync($users->random(3));
-
-
         $Developer = Specialization::create(['name' => ['ar' => 'مبرمج', 'en'    =>  'Developer']]);
-        // $Developer->users()->sync($users->random(3));
-
-
         $softEngineer = Specialization::create(['name' => ['ar' => 'مهندس برمجيات', 'en'    =>  'SoftWare Engineer']]);
-        // $softEngineer->users()->sync($users->random(3));
     }
 }

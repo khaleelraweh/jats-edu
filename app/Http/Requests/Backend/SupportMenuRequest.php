455d8cf563273401d4a14fce8e45c468c7a543d2
khaleel raweh <khaleelvisa@gmail.com>
2024-03-19 01:31:29 +0300

    support menu is ready

--- app/Http/Requests/Backend/SupportMenuRequest.php
+++ app/Http/Requests/Backend/SupportMenuRequest.php
@@ -26,7 +26,8 @@ class SupportMenuRequest extends FormRequest
         switch ($this->method()) {
             case 'POST': {
                     return [
-                        'title.*' => 'required|max:255|unique_translation:web_menus,title,NULL,section,section,' . $this->get('section'),
+                        // 'title.*' => 'required|max:255|unique_translation:web_menus,title,NULL,section,section,' . $this->get('section'),
+                        'title.*'       => 'required|max:255',
                         'link'          =>  'nullable',
                         'icon'          =>  'nullable',
                         'parent_id'     =>  'nullable',
@@ -46,7 +47,7 @@ class SupportMenuRequest extends FormRequest
             case 'PUT':
             case 'PATCH': {
                     return [
-                        'title.*' => 'required|unique_translation:web_menus,title,NULL,section,section,' . $this->get('section') . ',id,id' . $this->route()->support_menu,
+                        'title.*'           => 'required|max:255',
                         'link'              =>   'nullable',
                         'icon'              =>  'nullable',
                         'parent_id'         =>   'nullable',

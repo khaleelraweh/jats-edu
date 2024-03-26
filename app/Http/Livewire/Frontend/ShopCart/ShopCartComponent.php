05b5540818265ee94e9f50c23b15b4171add743a
khaleel raweh <khaleelvisa@gmail.com>
2024-03-27 01:24:28 +0300

    save all

--- app/Http/Livewire/Frontend/ShopCart/ShopCartComponent.php
+++ app/Http/Livewire/Frontend/ShopCart/ShopCartComponent.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Frontend\ShopCart;
 
+use App\Models\Course;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
@@ -18,13 +19,17 @@ class ShopCartComponent extends Component
     public $cartItems;
     public $totalPrice;
 
+    public $wishlistItems;
+
     public function render()
     {
         $this->cartItems = Cart::instance('default')->content();
         $this->totalPrice = currency_converter(Cart::total());
+        $this->wishlistItems = Cart::instance('wishlist')->content();
 
         return view('livewire.frontend.shop-cart.shop-cart-component', [
             'cartItems' => $this->cartItems,
+            'wishlistItems' => $this->wishlistItems,
             'totalPrice'    =>  $this->totalPrice,
         ]);
     }
@@ -39,5 +44,57 @@ class ShopCartComponent extends Component
         // Cart::instance('default')->remove($rowId);
         $this->emit('updateCartCount');
         $this->alert('success', __('panel.f_m_item_removed_from_shop_cart'));
+    }
+
+
+    public function removeFromWishlist($rowId)
+    {
+        $wishlist = Cart::instance('wishlist')->content()->where('rowId', $rowId);
+        if ($wishlist->isNotEmpty()) {
+            Cart::instance('wishlist')->remove($rowId);
+        }
+        $this->emit('updateCartCount');
+        $this->alert('success', __('panel.f_m_item_removed_from_wishlist_cart'));
+    }
+
+    public function moveToCart($rowId)
+    {
+        $item = Cart::instance('wishlist')->get($rowId);
+
+        $duplicates = Cart::instance('default')->search(function ($cartItem, $rId) use ($rowId) {
+            return $rId === $rowId;
+        });
+
+        if ($duplicates->isNotEmpty()) {
+
+            Cart::instance('wishlist')->remove($rowId);
+            $this->alert('error', __('panel.f_m_item_already_exist_in_shop_cart'));
+        } else {
+            Cart::instance('default')->add($item->id, $item->name, 1, $item->price)->associate(Course::class);
+            Cart::instance('wishlist')->remove($rowId);
+            $this->emit('updateCartCount');
+
+            $this->alert('success', __('panel.f_m_item_add_to_shop_cart'));
+        }
+    }
+    public function moveToWishlist($rowId)
+    {
+        $item = Cart::instance('default')->get($rowId);
+
+        $duplicates = Cart::instance('wishlist')->search(function ($cartItem, $rId) use ($rowId) {
+            return $rId === $rowId;
+        });
+
+        if ($duplicates->isNotEmpty()) {
+
+            Cart::instance('default')->remove($rowId);
+            $this->alert('error', __('panel.f_m_item_already_exist_in_wishlist_cart'));
+        } else {
+            Cart::instance('wishlist')->add($item->id, $item->name, 1, $item->price)->associate(Course::class);
+            Cart::instance('default')->remove($rowId);
+            $this->emit('updateCartCount');
+
+            $this->alert('success', __('panel.f_m_item_add_to_wishlist_cart'));
+        }
     }
 }

2d2d5f9efb2b613c4b46110d09d6e2ae6accbb2a
khaleel raweh <khaleelvisa@gmail.com>
2024-03-28 02:13:11 +0300

    save all

--- resources/views/frontend/shop-checkout.blade.php
+++ resources/views/frontend/shop-checkout.blade.php
@@ -131,6 +131,62 @@
                             </div>
                         </div>
 
+                        {{-- order view --}}
+                        <div id="order_reviews" class="woocommerce-checkout-review-order">
+                            <div class="woocommerce-checkout-review-order-inner">
+                                <h3 id="order_review_heading">Order details</h3>
+                                <table class="shop_table woocommerce-checkout-review-order-table">
+                                    <thead>
+                                        <tr>
+                                            <th class="product-name">Product</th>
+                                            <th class="product-total">Total</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr class="cart_item">
+                                            <td class="product-name">
+                                                Hoodie
+                                                <strong class="product-quantity">× 2</strong>
+                                            </td>
+                                            <td class="product-total">
+                                                <span class="woocommerce-Price-amount amount">
+                                                    <span class="woocommerce-Price-currencySymbol">$</span>
+                                                    59.00
+                                                </span>
+                                            </td>
+                                        </tr>
+                                        <tr class="cart_item">
+                                            <td class="product-name">
+                                                Seo Books
+                                                <strong class="product-quantity">× 1</strong>
+                                            </td>
+                                            <td class="product-total">
+                                                <span class="woocommerce-Price-amount amount">
+                                                    <span class="woocommerce-Price-currencySymbol">$</span>
+                                                    67.00
+                                                </span>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                    <tfoot>
+                                        <tr class="cart-subtotal">
+                                            <th>Subtotal</th>
+                                            <td><span class="woocommerce-Price-amount amount"><span
+                                                        class="woocommerce-Price-currencySymbol">$</span>109.95</span></td>
+                                        </tr>
+
+                                        <tr class="order-total">
+                                            <th>Total</th>
+                                            <td><strong><span class="woocommerce-Price-amount amount"><span
+                                                            class="woocommerce-Price-currencySymbol">$</span>109.95</span></strong>
+                                            </td>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+
+                        </div>
+
                     </div>
                 </div>
 
@@ -139,19 +195,13 @@
 
             <div id="order_review" class="woocommerce-checkout-review-order">
                 <div class="woocommerce-checkout-review-order-inner">
-                    <h3 id="order_review_heading">Your order</h3>
+                    <h3 id="order_review_heading">Summary</h3>
                     <table class="shop_table woocommerce-checkout-review-order-table">
-                        <thead>
-                            <tr>
-                                <th class="product-name">Product</th>
-                                <th class="product-total">Total</th>
-                            </tr>
-                        </thead>
+
                         <tbody>
                             <tr class="cart_item">
                                 <td class="product-name">
-                                    Hoodie
-                                    <strong class="product-quantity">× 2</strong>
+                                    Original Price:
                                 </td>
                                 <td class="product-total">
                                     <span class="woocommerce-Price-amount amount">
@@ -162,8 +212,7 @@
                             </tr>
                             <tr class="cart_item">
                                 <td class="product-name">
-                                    Seo Books
-                                    <strong class="product-quantity">× 1</strong>
+                                    Discounts:
                                 </td>
                                 <td class="product-total">
                                     <span class="woocommerce-Price-amount amount">
@@ -175,9 +224,6 @@
                         </tbody>
                         <tfoot>
                             <tr class="cart-subtotal">
-                                <th>Subtotal</th>
-                                <td><span class="woocommerce-Price-amount amount"><span
-                                            class="woocommerce-Price-currencySymbol">$</span>109.95</span></td>
                             </tr>
 
                             <tr class="order-total">
@@ -188,110 +234,64 @@
                             </tr>
                         </tfoot>
                     </table>
-                </div>
-
-                <div id="payment" class="woocommerce-checkout-payment">
-                    <ul class="wc_payment_methods payment_methods methods">
-                        <li class="wc_payment_method payment_method_bacs">
-                            <input id="payment_method_bacs" type="radio" class="input-radio" name="payment_method"
-                                value="bacs" data-order_button_text="">
-
-                            <label for="payment_method_bacs">Direct bank transfer </label>
-                            <div class="payment_box payment_method_bacs" style="display: block;">
-                                <p>Make your payment directly into our bank account. Please use your Order ID as the payment
-                                    reference. Your order won’t be shipped until the funds have cleared in our account.</p>
-                            </div>
-                        </li>
-
-                        <li class="wc_payment_method payment_method_cheque">
-                            <input id="payment_method_cheque" type="radio" class="input-radio" name="payment_method"
-                                value="cheque" data-order_button_text="">
-
-                            <label for="payment_method_cheque">Check payments </label>
-                            <div class="payment_box payment_method_cheque d-none" style="display: block;">
-                                <p>Please send a check to Store Name, Store Street, Store Town, Store State / County, Store
-                                    Postcode.</p>
-                            </div>
-                        </li>
-
-                        <li class="wc_payment_method payment_method_cod">
-                            <input id="payment_method_cod" type="radio" class="input-radio" name="payment_method"
-                                value="cod" checked="checked" data-order_button_text="">
-
-                            <label for="payment_method_cod">Cash on delivery </label>
-                            <div class="payment_box payment_method_cod d-none" style="display: block;">
-                                <p>Pay with cash upon delivery.</p>
-                            </div>
-                        </li>
-
-                        <li class="wc_payment_method payment_method_paypal">
-                            <input id="payment_method_paypal" type="radio" class="input-radio" name="payment_method"
-                                value="paypal" data-order_button_text="Proceed to PayPal">
-
-                            <label for="payment_method_paypal">PayPal
-                                <img src="../../../www.paypalobjects.com/webstatic/mktg/Logo/AM_mc_vs_ms_ae_UK.png"
-                                    alt="PayPal acceptance mark">
-                                <a href="https://www.paypal.com/gb/webapps/mpp/paypal-popup" class="about_paypal">What
-                                    is
-                                    PayPal?</a>
-                            </label>
-
-                            <div class="payment_box payment_method_paypal d-none" style="display: block;">
-                                <p>Pay via PayPal; you can pay with your credit card if you don’t have a PayPal account.</p>
-                            </div>
-                        </li>
-                    </ul>
                     <div class="form-row place-order">
                         <a href="shop-order-completed.html" class="btn btn-primary btn-block">
-                            PLACE ORDER
+                            proccess
                         </a>
                     </div>
                 </div>
+                <div class="form-row place-order d-none">
+                    <a href="shop-order-completed.html" class="btn btn-primary btn-block">
+                        proccess
+                    </a>
+                </div>
             </div>
         </form>
-    </div>
 
 
-    <!-- CALL ACTION -->
-    <section class="py-6 py-md-11 border-top border-bottom" data-jarallax data-speed=".8"
-        style="background-image: url(assets/img/illustrations/illustration-1.jpg)">
-        <div class="container text-center py-xl-4" data-aos="fade-up">
-            <div class="row">
-                <div class="col-xl-7 mx-auto">
-                    <h1 class="text-capitalize">Subscribe our newsletter</h1>
-                    <p class="text-capitalize font-size-lg mb-md-6 mb-4">Your download should start automatically, if not
-                        Click here. Should I give up, huh?.</p>
-                    <div class="mx-md-8">
-                        <form>
-                            <div class="input-group">
-                                <input type="text" class="form-control" placeholder="Enter your email"
-                                    aria-label="Enter your email" aria-describedby="button-addon2">
-                                <div class="input-group-append">
-                                    <button class="btn btn-primary btn-sm-wide" type="button"
-                                        id="button-addon2">Subscribe</button>
+
+
+        <!-- CALL ACTION -->
+        <section class="py-6 py-md-11 border-top border-bottom" data-jarallax data-speed=".8"
+            style="background-image: url(assets/img/illustrations/illustration-1.jpg)">
+            <div class="container text-center py-xl-4" data-aos="fade-up">
+                <div class="row">
+                    <div class="col-xl-7 mx-auto">
+                        <h1 class="text-capitalize">Subscribe our newsletter</h1>
+                        <p class="text-capitalize font-size-lg mb-md-6 mb-4">Your download should start automatically, if
+                            not
+                            Click here. Should I give up, huh?.</p>
+                        <div class="mx-md-8">
+                            <form>
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Enter your email"
+                                        aria-label="Enter your email" aria-describedby="button-addon2">
+                                    <div class="input-group-append">
+                                        <button class="btn btn-primary btn-sm-wide" type="button"
+                                            id="button-addon2">Subscribe</button>
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-@endsection
+        </section>
+    @endsection
 
-@section('script')
-    <!-- Script to toggle accordion panels based on radio button change -->
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var radios = document.querySelectorAll('.accordion-radio');
-            radios.forEach(function(radio) {
-                radio.addEventListener('change', function() {
-                    var target = this.getAttribute('data-bs-target');
-                    var collapse = document.querySelector(target);
-                    var bsCollapse = new bootstrap.Collapse(collapse);
-                    bsCollapse.toggle();
+    @section('script')
+        <!-- Script to toggle accordion panels based on radio button change -->
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                var radios = document.querySelectorAll('.accordion-radio');
+                radios.forEach(function(radio) {
+                    radio.addEventListener('change', function() {
+                        var target = this.getAttribute('data-bs-target');
+                        var collapse = document.querySelector(target);
+                        var bsCollapse = new bootstrap.Collapse(collapse);
+                        bsCollapse.toggle();
+                    });
                 });
             });
-        });
-    </script>
-@endsection
+        </script>
+    @endsection

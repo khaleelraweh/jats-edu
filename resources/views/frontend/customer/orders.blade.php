06883fe95ce88f427c76f080378b70eea1fcf96a
khaleel raweh <khaleelvisa@gmail.com>
2024-04-21 11:36:15 +0300

    save all

--- resources/views/frontend/customer/orders.blade.php
+++ resources/views/frontend/customer/orders.blade.php
@@ -1,35 +1,31 @@
 @extends('layouts.app')
 @section('content')
-    <header class="bg-white py-5 py-lg-5 position-relative mb-5" style="background-image: none;">
-        <div class="container text-center py-xl-5">
+    <!-- PAGE TITLE -->
+    <header class="py-6 py-md-8" style="background-image: none;">
+        <div class="container text-center py-xl-2">
             <h1 class="display-4 fw-semi-bold mb-0">
-                <a href="{{ route('customer.orders') }}">
-                    {{ __('transf.txt_my_orders') }}
-                </a>
+                {{ __('transf.txt_my_orders') }}
             </h1>
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb breadcrumb-scroll justify-content-center">
                     <li class="breadcrumb-item">
-                        <a class="text-gray-800" href="{{ route('frontend.index') }}">
-                            {{ __('transf.lnk_home') }}
+                        <a class="text-gray-800" href="#">
+                            <a class="text-gray-800" href="{{ route('frontend.index') }}">
+                                {{ __('transf.lnk_home') }}
+                            </a>
                         </a>
                     </li>
                     <li class="breadcrumb-item text-gray-800 active" aria-current="page">
-                        <a href="{{ route('customer.orders') }}">
-                            {{ __('transf.txt_orders') }}
-                        </a>
-
+                        {{ __('transf.txt_orders') }}
                     </li>
                 </ol>
             </nav>
         </div>
         <!-- Img -->
-
-
-
-        <img class="position-absolute position-center-y right-0 mw-300p mw-xl-450p me-md-6 d-none d-lg-block img-fluid"
-            src="{{ asset('assets/courses/il1.png') }}" alt="...">
+        <img class="d-none img-fluid" src="...html" alt="...">
     </header>
+
+
 
     <section class="py-5">
         <div class="row m-0">
--- resources/views/frontend/customer/profile.blade.php
+++ resources/views/frontend/customer/profile.blade.php
@@ -1,32 +1,27 @@
 @extends('layouts.app')
 @section('content')
-    <header class="bg-white py-5 py-lg-5 position-relative mb-5" style="background-image: none;">
-        <div class="container text-center py-xl-5">
+    <header class="py-6 py-md-8" style="background-image: none;">
+        <div class="container text-center py-xl-2">
             <h1 class="display-4 fw-semi-bold mb-0">
-                <a href="{{ route('customer.profile') }}">
-                    {{ __('transf.txt_profile') }}
-                </a>
+                {{ __('transf.txt_profile') }}
             </h1>
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb breadcrumb-scroll justify-content-center">
                     <li class="breadcrumb-item">
-                        <a class="text-gray-800" href="{{ route('frontend.index') }}">
-                            {{ __('transf.lnk_home') }}
+                        <a class="text-gray-800" href="#">
+                            <a class="text-gray-800" href="{{ route('frontend.index') }}">
+                                {{ __('transf.lnk_home') }}
+                            </a>
                         </a>
                     </li>
                     <li class="breadcrumb-item text-gray-800 active" aria-current="page">
-                        <a href="{{ route('customer.profile') }}">
-                            {{ __('transf.txt_profile') }}
-                        </a>
-
+                        {{ __('transf.txt_profile') }}
                     </li>
                 </ol>
             </nav>
         </div>
         <!-- Img -->
-
-        <img class="position-absolute position-center-y right-0 mw-300p mw-xl-450p  d-none d-lg-block img-fluid"
-            src="{{ asset('assets/courses/il1.png') }}" alt="...">
+        <img class="d-none img-fluid" src="...html" alt="...">
     </header>
 
     <section class="py-5">

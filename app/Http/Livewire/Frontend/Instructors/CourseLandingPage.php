a17f1c65150c47df66c9a6e1666ca4672603ef3f
khaleel raweh <khaleelvisa@gmail.com>
2024-05-07 15:43:45 +0300

    save all

--- app/Http/Livewire/Frontend/Instructors/CourseLandingPage.php
+++ app/Http/Livewire/Frontend/Instructors/CourseLandingPage.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Livewire\Frontend\Instructors;
 
+use App\Models\Course;
+use App\Models\CourseCategory;
+use App\Models\User;
 use Livewire\Component;
 
 class CourseLandingPage extends Component
 {
+    public $courseId;
+
+    public function mount($courseId)
+    {
+        $this->courseId = $courseId;
+        $course = Course::where('id', $this->courseId)->first();
+    }
+
     public function render()
     {
-        return view('livewire.frontend.instructors.course-landing-page');
+        $course = Course::where('id', $this->courseId)->first();
+        $course_categories = CourseCategory::whereStatus(1)->get(['id', 'title']);
+
+        // Get active instructor
+        $instructors = User::whereHas('roles', function ($query) {
+            $query->where('name', 'instructor');
+        })->active()->get(['id', 'first_name', 'last_name']);
+
+
+        $courseinstructors = $course->users->pluck(['id'])->toArray();
+
+        return view('livewire.frontend.instructors.course-landing-page', compact('course_categories', 'course', 'instructors', 'courseinstructors'));
     }
 }

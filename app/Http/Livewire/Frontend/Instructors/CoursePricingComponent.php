755e8b42df9f388d76b3e5f0574b08c758296bf5
khaleel raweh <khaleelvisa@gmail.com>
2024-05-08 18:45:26 +0300

    save all

--- app/Http/Livewire/Frontend/Instructors/CoursePricingComponent.php
+++ app/Http/Livewire/Frontend/Instructors/CoursePricingComponent.php
@@ -8,14 +8,44 @@ use Livewire\Component;
 class CoursePricingComponent extends Component
 {
     public $courseId;
+    public $price;
+    public $offer_price;
+    public $offer_ends;
+
+    protected $rules = [
+        'price' => ['required', 'numeric', 'min:0'],
+        'offer_price' => ['nullable', 'numeric', 'min:0'],
+        // 'offer_ends' => ['required', 'date'],
+    ];
+
 
 
     public function mount($courseId)
     {
         $this->courseId = $courseId;
-        $course = Course::where('id', $this->courseId)->first();
+        $course = Course::findOrFail($this->courseId);
+        $this->price = $course->price;
+        $this->offer_price = $course->offer_price;
+        $this->offer_ends = $course->offer_ends;
     }
 
+
+    public function save()
+    {
+        $this->validate();
+
+        $course = Course::findOrFail($this->courseId);
+        $course->update([
+            'price' => $this->price,
+            'offer_price' => $this->offer_price,
+            'offer_ends' => $this->offer_ends,
+        ]);
+
+        // session()->flash('success', 'Course pricing information updated successfully.');
+
+
+        // You can redirect to another page or emit an event if needed
+    }
 
     public function render()
     {
--- resources/views/livewire/frontend/instructors/course-pricing-component.blade.php
+++ resources/views/livewire/frontend/instructors/course-pricing-component.blade.php
@@ -1,11 +1,11 @@
 <div>
-    <form action="">
+    <form wire:submit.prevent="save">
         {{-- course price  --}}
         <div class="row">
             <div class="col-md-12 col-sm-12 pt-3">
                 <label for="price"> {{ __('panel.price') }} </label>
-                <input type="text" name="price" id="price" value="{{ old('price', $course->price) }}"
-                    class="form-control">
+                <input type="text" name="price" wire:model.defer="price" id="price"
+                    value="{{ old('price', $course->price) }}" class="form-control">
                 @error('price')
                     <span class="text-danger">{{ $message }}</span>
                 @enderror
@@ -16,7 +16,7 @@
         <div class="row">
             <div class="col-md-12 col-sm-12 pt-3">
                 <label for="offer_price"> {{ __('panel.offer_price') }} </label>
-                <input type="text" id="offer_price" name="offer_price"
+                <input type="text" id="offer_price" name="offer_price" wire:model.defer="offer_price"
                     value="{{ old('offer_price', $course->offer_price) }}" class="form-control">
                 @error('offer_price')
                     <span class="text-danger">{{ $message }}</span>
@@ -30,12 +30,18 @@
                 <label for="offer_ends" class="control-label"><span> {{ __('panel.offer_ends') }}
                     </span><span class="require red">*</span></label>
                 <div class="form-group">
-                    <input type="text" id="offer_ends" name="offer_ends"
+                    <input type="text" id="offer_ends" name="offer_ends" wire:model.defer="offer_ends"
                         value="{{ old('offer_ends', $course->offer_ends) }}" class="form-control">
                     @error('offer_ends')
                         <span class="text-danger">{{ $message }}</span>
                     @enderror
                 </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12 pt-3">
+                <button type="submit" class="btn btn-primary">Save</button>
             </div>
         </div>
     </form>

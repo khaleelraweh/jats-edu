ce4e2e6bea7e347412d6fbd3d81f0efe5bf78144
khaleel raweh <khaleelvisa@gmail.com>
2024-05-12 15:10:03 +0300

    save all

--- app/Http/Livewire/Frontend/Instructors/CoursePublishDataComponent.php
+++ app/Http/Livewire/Frontend/Instructors/CoursePublishDataComponent.php
@@ -3,16 +3,38 @@
 namespace App\Http\Livewire\Frontend\Instructors;
 
 use App\Models\Course;
+use DateTimeImmutable;
 use Livewire\Component;
 
 class CoursePublishDataComponent extends Component
 {
     public $courseId;
+    public $published_on;
+    public $published_on_time;
+    public $status;
+
 
     public function mount($courseId)
     {
         $this->courseId = $courseId;
         $course = Course::findOrFail($this->courseId);
+    }
+
+    public function save()
+    {
+
+        $course = Course::findOrFail($this->courseId);
+
+        $published = $this->published_on . ' ' . $this->published_on_time;
+        $published = new DateTimeImmutable($published);
+
+        $course->update([
+            'published_on' => $published,
+            'status' => $this->status
+        ]);
+
+
+        // You can redirect to another page or emit an event if needed
     }
 
     public function render()
--- resources/views/livewire/frontend/instructors/course-publish-data-component.blade.php
+++ resources/views/livewire/frontend/instructors/course-publish-data-component.blade.php
@@ -1,11 +1,11 @@
 <div>
-    <form action="">
+    <form wire:submit.prevent="save">
         {{-- published_on and published_on_time  --}}
         <div class="row">
             <div class="col-sm-12 col-md-12 pt-4">
                 <div class="form-group">
                     <label for="published_on"> {{ __('panel.published_date') }}</label>
-                    <input type="text" id="published_on" name="published_on"
+                    <input type="text" id="published_on" name="published_on" wire:model.defer="published_on"
                         value="{{ old('published_on', \Carbon\Carbon::parse($course->published_on)->Format('Y-m-d')) }}"
                         class="form-control">
                     @error('published_on')
@@ -20,6 +20,7 @@
                 <div class="form-group">
                     <label for="published_on_time">{{ __('panel.published_time') }}</label>
                     <input type="text" id="published_on_time" name="published_on_time"
+                        wire:model.defer="published_on_time"
                         value="{{ old('published_on_time', \Carbon\Carbon::parse($course->published_on)->Format('h:i A')) }}"
                         class="form-control">
                     @error('published_on_time')
@@ -35,7 +36,7 @@
                 <label for="status" class="control-label col-md-2 col-sm-12 ">
                     <span>{{ __('panel.status') }}</span>
                 </label>
-                <select name="status" class="form-control">
+                <select name="status" class="form-control" wire:model.defer="status">
                     <option value="1" {{ old('status', $course->status) == '1' ? 'selected' : null }}>
                         {{ __('panel.status_active') }}
                     </option>
@@ -46,6 +47,12 @@
                 @error('status')
                     <span class="text-danger">{{ $message }}</span>
                 @enderror
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12 pt-3">
+                <button type="submit" class="btn btn-primary">Save</button>
             </div>
         </div>
     </form>

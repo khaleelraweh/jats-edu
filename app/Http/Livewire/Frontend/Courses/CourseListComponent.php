c75fa8c4e6844a669442a5b98fbb3282be9b716b
khaleel raweh <khaleelvisa@gmail.com>
2024-05-13 14:58:29 +0300

    save all

--- app/Http/Livewire/Frontend/Courses/CourseListComponent.php
+++ app/Http/Livewire/Frontend/Courses/CourseListComponent.php
@@ -50,6 +50,28 @@ class CourseListComponent extends Component
 
     public function render()
     {
+        // $instructors_menu = User::whereHasRoles('instructor')->hasCourses()
+        //     ->orderBy('first_name')
+        //     ->orderBy('last_name')
+        //     ->when($this->searchQuery, function ($query) {
+        //         $query->where(function ($subQuery) {
+        //             $subQuery->where('first_name', 'LIKE', '%' . $this->searchQuery . '%')
+        //                 ->orWhere('last_name', 'LIKE', '%' . $this->searchQuery . '%');
+        //         });
+        //     })
+        //     ->withCount('courses') // Load the count of associated courses for each instructor
+        //     ->get()
+        //     ->groupBy(function ($user) {
+        //         return $user->first_name . ' ' . $user->last_name;
+        //     })
+        //     ->map(function ($group) {
+        //         // Calculate the count of instructors in each group
+        //         $instructorsCount = $group->count();
+        //         // Calculate the sum of courses count for each instructor group
+        //         $coursesCount = $group->sum('courses_count');
+        //         return compact('instructorsCount', 'coursesCount');
+        // });
+
         $instructors_menu = User::whereHasRoles('instructor')->hasCourses()
             ->orderBy('first_name')
             ->orderBy('last_name')
@@ -59,7 +81,9 @@ class CourseListComponent extends Component
                         ->orWhere('last_name', 'LIKE', '%' . $this->searchQuery . '%');
                 });
             })
-            ->withCount('courses') // Load the count of associated courses for each instructor
+            ->withCount(['courses' => function ($query) {
+                $query->where('section', 1); // Count only courses where section is 1
+            }])
             ->get()
             ->groupBy(function ($user) {
                 return $user->first_name . ' ' . $user->last_name;
@@ -71,6 +95,7 @@ class CourseListComponent extends Component
                 $coursesCount = $group->sum('courses_count');
                 return compact('instructorsCount', 'coursesCount');
             });
+
 
 
 

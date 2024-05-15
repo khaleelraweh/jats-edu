08dc9a7f2071a0c2fa1b508a74b3135552795c17
khaleel raweh <khaleelvisa@gmail.com>
2024-05-15 15:27:30 +0300

    save all

--- app/Http/Livewire/Frontend/Instructors/CourseDetailsConfirmationComponent.php
+++ app/Http/Livewire/Frontend/Instructors/CourseDetailsConfirmationComponent.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Livewire\Frontend\Instructors;
 
+use App\Models\Course;
 use Livewire\Component;
 
 class CourseDetailsConfirmationComponent extends Component
 {
+
+    public $courseId;
+
     public function render()
     {
-        return view('livewire.frontend.instructors.course-details-confirmation-component');
+        $course = Course::where('id', $this->courseId)->first();
+        return view('livewire.frontend.instructors.course-details-confirmation-component', compact('course'));
     }
 }

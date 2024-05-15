e85737eeccd355e19968af7e4000c710a0286c1d
khaleel raweh <khaleelvisa@gmail.com>
2024-05-15 16:37:07 +0300

    save all

--- resources/views/livewire/frontend/instructors/course-details-confirmation-component.blade.php
+++ resources/views/livewire/frontend/instructors/course-details-confirmation-component.blade.php
@@ -9,13 +9,17 @@
     <div class="row justify-content-center">
         <div class="col-lg-6">
             <div class="text-center">
-                <div class="mb-4">
-                    <i class="mdi mdi-check-circle-outline text-success display-4"></i>
-                </div>
-                <div>
-                    <h5>{{ __('transf.Course Data Confirmed successfully') }}</h5>
-                    <p class="text-muted">{{ __('transf.Course Data Confirmed successfully tips') }}</p>
-                </div>
+                @if ($databaseDataValid && !$errors->any())
+                    <div class="mb-4">
+                        <i class="mdi mdi-check-circle-outline text-success display-4"></i>
+                    </div>
+                    <div>
+
+                        <h5>{{ __('transf.Course Data Confirmed successfully') }}</h5>
+                        <p class="text-muted">{{ __('transf.Course Data Confirmed successfully tips') }}</p>
+
+                    </div>
+                @endif
             </div>
         </div>
     </div>

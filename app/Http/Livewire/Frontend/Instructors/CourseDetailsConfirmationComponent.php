28d1a18a406a4a316833cd93821049887fb859af
khaleel raweh <khaleelvisa@gmail.com>
2024-05-15 15:53:48 +0300

    save all

--- app/Http/Livewire/Frontend/Instructors/CourseDetailsConfirmationComponent.php
+++ app/Http/Livewire/Frontend/Instructors/CourseDetailsConfirmationComponent.php
@@ -3,12 +3,63 @@
 namespace App\Http\Livewire\Frontend\Instructors;
 
 use App\Models\Course;
+use Illuminate\Support\Facades\Validator;
 use Livewire\Component;
 
 class CourseDetailsConfirmationComponent extends Component
 {
 
     public $courseId;
+
+    // General validation 
+    public $databaseDataValid           = false;
+
+    public $courseLandingTabValid       = false;
+    public $courseObjectiveTabValid     = false;
+    public $courseCurriculumTabValid    = false;
+    public $coursePricingTabValid       = false;
+    public $coursePublishedTabValid     = false;
+
+
+    // Field validation 
+    public $priceValid = false;
+
+
+    protected $rules = [
+        'price' => ['required', 'numeric', 'min:0'],
+    ];
+
+
+
+    public function mount($courseId)
+    {
+        $this->validateDatabaseData();
+    }
+
+
+
+    protected function validateDatabaseData()
+    {
+        $course = Course::where('id', $this->courseId)->first();
+
+
+        // ===================== Start Price Tab Validation ===============//
+        // Validate price 
+        $priceValid = true;
+        $validator = Validator::make(['price' => $course->price], [
+            'price' => ['required', 'numeric', 'min:0'],
+        ]);
+        if ($validator->fails()) {
+            $priceValid = false;
+        }
+
+        $this->priceValid = $priceValid;
+        $this->coursePricingTabValid = $priceValid;
+
+        // ===================== End Price Tab Validation ===============//
+
+        $this->databaseDataValid = $this->coursePricingTabValid;
+    }
 
     public function render()
     {

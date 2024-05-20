7897e9d3ee5ba80e8766bb13e97cd461aa57fbc2
khaleel raweh <khaleelvisa@gmail.com>
2024-05-20 21:54:36 +0300

    save all

--- resources/views/livewire/frontend/courses/add-to-cart-component.blade.php
+++ resources/views/livewire/frontend/courses/add-to-cart-component.blade.php
@@ -1,29 +1,52 @@
 <div>
 
     @php
-        $duplicates = Cart::instance('default')->search(function ($cartItem, $rowId) use ($courseId) {
-            return $cartItem->id === $courseId;
-        });
+        $userId = auth()->id();
+
+        $course = App\Models\Course::whereId($courseId)->firstOrFail();
+
+        // Check if the user is already enrolled in the course
+        $isEnrolled = App\Models\Order::where('user_id', $userId)
+            ->whereHas('courses', function ($query) use ($courseId) {
+                $query->where('course_id', $courseId);
+            })
+            ->exists();
+
     @endphp
 
-    @if ($duplicates->isNotEmpty())
-        <a href="{{ route('frontend.shop_cart') }}" class="btn btn-primary btn-block mb-3">
-            {{ __('transf.btn_go_to_cart') }}
-        </a>
-        <a href="{{ route('frontend.shop_checkout') }}"
-            class=" btn btn-teal btn-block text-white mb-3">{{ __('transf.btn_buy_now') }}</a>
+    @if ($isEnrolled)
+        <a href="{{ route('customer.lesson_single', $course->slug) }}"
+            class="btn btn-blue btn-block mb-6">{{ __('transf.btn_go_to_course') }}</a>
     @else
-        {{-- Add to cart --}}
-        <button wire:click.prevent="addToCart()" class="btn btn-primary btn-block mb-3" type="button" name="button">
-            {{ __('transf.btn_add_to_cart') }}
-        </button>
+        @php
+            $duplicates = Cart::instance('default')->search(function ($cartItem, $rowId) use ($courseId) {
+                return $cartItem->id === $courseId;
+            });
+        @endphp
 
-        {{-- Buy Now --}}
-        <button wire:click.prevent="BuyNow()" class="btn btn-teal btn-block text-white mb-3" type="button"
-            name="button">
-            {{ __('transf.btn_buy_now') }}
-        </button>
+        @if ($duplicates->isNotEmpty())
+            <a href="{{ route('frontend.shop_cart') }}" class="btn btn-primary btn-block mb-3">
+                {{ __('transf.btn_go_to_cart') }}
+            </a>
+            <a href="{{ route('frontend.shop_checkout') }}"
+                class=" btn btn-teal btn-block text-white mb-3">{{ __('transf.btn_buy_now') }}</a>
+        @else
+            {{-- Add to cart --}}
+            <button wire:click.prevent="addToCart()" class="btn btn-primary btn-block mb-3" type="button" name="button">
+                {{ __('transf.btn_add_to_cart') }}
+            </button>
+
+            {{-- Buy Now --}}
+            <button wire:click.prevent="BuyNow()" class="btn btn-teal btn-block text-white mb-3" type="button"
+                name="button">
+                {{ __('transf.btn_buy_now') }}
+            </button>
+        @endif
+
     @endif
+
+
+
 
 
 

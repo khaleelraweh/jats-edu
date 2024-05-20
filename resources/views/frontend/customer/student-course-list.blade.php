8e57d6898c19ecb11116d97b5a7c2eadf8cf2277
khaleel raweh <khaleelvisa@gmail.com>
2024-05-20 14:58:59 +0300

    save all

--- resources/views/frontend/customer/student-course-list.blade.php
+++ resources/views/frontend/customer/student-course-list.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
     <!-- PAGE TITLE
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    ================================================== -->
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ================================================== -->
     <header class="bg-white py-5 py-lg-5 position-relative mb-5" style="background-image: none;">
         <div class="container text-center py-xl-5">
             <h1 class="display-4 fw-semi-bold mb-0">
-                <a href="{{ route('frontend.courses') }}">
+                <a href="{{ route('customer.courses') }}">
                     {{ __('transf.txt_my_learning_courses') }}
                 </a>
             </h1>
@@ -18,7 +18,7 @@
                         </a>
                     </li>
                     <li class="breadcrumb-item text-gray-800 active" aria-current="page">
-                        <a href="{{ route('frontend.courses') }}">
+                        <a href="{{ route('customer.courses') }}">
                             {{ __('transf.txt_my_learning_courses') }}
                         </a>
 

82834de586fcd8be3bc207a7779785aafaf2914b
khaleel raweh <khaleelvisa@gmail.com>
2024-05-26 15:38:44 +0300

    save all

--- resources/views/frontend/shop-checkoutChange.blade.php
+++ resources/views/frontend/shop-checkoutChange.blade.php
@@ -35,41 +35,12 @@
                         <h3>{{ __('transf.billing_details') }}</h3>
 
                         <div id="accordionCurriculum">
-                            <!-- Accordion panel 1 -->
-                            <div class="border rounded mb-1 overflow-hidden">
-                                <!-- Accordion heading as radio button -->
-                                <div class="d-flex align-items-center" id="curriculumheadingOne">
-                                    <h5 class="mb-0 w-100">
-                                        <label
-                                            class="d-flex align-items-center p-5 min-height-80 text-dark fw-medium collapse-accordion-toggle line-height-one payment-card-head">
-                                            <input type="radio" class="accordion-radio me-4 text-dark d-flex"
-                                                name="paymentMethod" value="paypal" data-bs-toggle="collapse"
-                                                data-bs-target="#CurriculumcollapseOne" aria-expanded="false"
-                                                aria-controls="CurriculumcollapseOne">
 
-                                            <i class="fab fa-paypal mx-2"></i>
-                                            {{ __('transf.paypal') }}
-                                        </label>
-                                    </h5>
-                                </div>
-                                <!-- Accordion content -->
-                                <div id="CurriculumcollapseOne" class="collapse" aria-labelledby="curriculumheadingOne"
-                                    data-bs-parent="#accordionCurriculum">
-                                    <!-- Content for PayPal -->
-                                    <div class="border-top px-5 py-4 min-height-70 d-md-flex align-items-center">
-                                        <div class="d-flex align-items-center me-auto mb-4 mb-md-0">
-                                            <div class="ms-4">
-                                                <p>{{ __('transf.paypal_details') }}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
 
-                            <!-- Accordion panel 2 -->
+                            <!-- Accordion panel 3 -->
                             <div class="border rounded shadow mb-6 overflow-hidden">
                                 <!-- Accordion heading as radio button -->
-                                <div class="d-flex align-items-center" id="curriculumheadingTwo">
+                                <div class="d-flex align-items-center" id="curriculumheadingThree">
                                     <h5 class="mb-0 w-100">
 
                                         <label
@@ -78,8 +49,8 @@
                                             <label class="d-flex payment-card-head">
                                                 <input type="radio" class="accordion-radio me-4 text-dark d-flex"
                                                     name="paymentMethod" value="creditDebit" data-bs-toggle="collapse"
-                                                    data-bs-target="#CurriculumcollapseTwo" aria-expanded="false"
-                                                    aria-controls="CurriculumcollapseTwo">
+                                                    data-bs-target="#CurriculumcollapseThree" aria-expanded="false"
+                                                    aria-controls="CurriculumcollapseThree">
                                                 <i class="fas fa-credit-card mx-2"></i>
                                                 {{ __('transf.credit_debit_card') }}
                                             </label>
@@ -93,13 +64,10 @@
                                             </label>
 
                                         </label>
-
-
-
                                     </h5>
                                 </div>
                                 <!-- Accordion content -->
-                                <div id="CurriculumcollapseTwo" class="collapse" aria-labelledby="curriculumheadingTwo"
+                                <div id="CurriculumcollapseThree" class="collapse" aria-labelledby="curriculumheadingThree"
                                     data-bs-parent="#accordionCurriculum">
                                     <!-- Content for Credit/Debit Card -->
                                     <div class="border-top px-5 py-4 min-height-70 d-md-flex align-items-center">
@@ -128,10 +96,9 @@
                                                         id="billing_first_name_field" data-priority="10">
                                                         <label for="billing_expiry_date" class="">Expiry date <abbr
                                                                 class="required" title="required">*</abbr></label>
-                                                        <input type="text" class="input-text "
-                                                            name="billing_expiry_date" id="billing_expiry_date"
-                                                            placeholder="MM/YY" value="" autocomplete="expiry_date"
-                                                            autofocus="autofocus">
+                                                        <input type="text" class="input-text " name="billing_expiry_date"
+                                                            id="billing_expiry_date" placeholder="MM/YY" value=""
+                                                            autocomplete="expiry_date" autofocus="autofocus">
                                                     </p>
                                                     <p class="form-row form-row-last validate-required"
                                                         id="billing_last_name_field" data-priority="20">
@@ -141,73 +108,6 @@
                                                             id="billing_cvc" placeholder="CVC" value=""
                                                             autocomplete="CVC">
                                                     </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <!-- Accordion panel 3 -->
-                            <div class="border rounded shadow mb-6 overflow-hidden">
-                                <!-- Accordion heading as radio button -->
-                                <div class="d-flex align-items-center" id="curriculumheadingThree">
-                                    <h5 class="mb-0 w-100">
-
-                                        <label
-                                            class="d-flex align-items-center justify-content-between p-5 min-height-80 text-dark fw-medium collapse-accordion-toggle line-height-one">
-
-                                            <label class="d-flex payment-card-head">
-                                                <input type="radio" class="accordion-radio me-4 text-dark d-flex"
-                                                    name="paymentMethod" value="kurimaBank" data-bs-toggle="collapse"
-                                                    data-bs-target="#CurriculumcollapseThree" aria-expanded="false"
-                                                    aria-controls="CurriculumcollapseThree">
-                                                <i class="fas fa-credit-card mx-2"></i>
-                                                {{ __('transf.burima Bank') }}
-                                            </label>
-
-
-                                            <label class="payment-card-icons">
-                                                <i class="fab fa-cc-visa"></i>
-                                                <i class="fab fa-cc-mastercard"></i>
-                                                <i class="fab fa-cc-amex"></i>
-                                                <i class="fab fa-cc-discover"></i>
-                                            </label>
-
-                                        </label>
-                                    </h5>
-                                </div>
-                                <!-- Accordion content -->
-                                <div id="CurriculumcollapseThree" class="collapse"
-                                    aria-labelledby="curriculumheadingThree" data-bs-parent="#accordionCurriculum">
-                                    <!-- Content for Kurima Bank  -->
-                                    <div class="border-top px-5 py-4 min-height-70 d-md-flex align-items-center">
-                                        <div class="d-flex align-items-center me-auto mb-4 mb-md-0">
-                                            <div class="ms-4">
-                                                <div class="woocommerce-billing-fields__field-wrapper">
-                                                    <p class="form-row form-row-last validate-required validate-email"
-                                                        id="billing_email_field" data-priority="110">
-                                                        <label for="billing_number" class="">
-                                                            {{ __('panel.f_customer_bank_account_number') }}
-                                                            <abbr class="required" title="required">*</abbr>
-                                                        </label>
-                                                        <input type="text" class="input-text " name="bankAccNumber"
-                                                            id="billing_number" placeholder="1234 5678 9012 3456"
-                                                            value="" autocomplete="number">
-                                                    </p>
-
-
-                                                    <p class="form-row form-row-last validate-required validate-email"
-                                                        id="billing_email_field" data-priority="110">
-                                                        <label for="bankReceipt" class="">
-                                                            {{ __('panel.f_copy_of_the_transfer_receipt') }}
-                                                            <abbr class="required" title="required">*</abbr>
-                                                        </label>
-
-                                                        <input type="file" name="bankReceipt" id="bankReceipt"
-                                                            class="form-control input-text form-control--sm ">
-                                                    </p>
-
                                                 </div>
                                             </div>
                                         </div>

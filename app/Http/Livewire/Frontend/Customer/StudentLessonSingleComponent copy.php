6cae8cc733045ed784563bde034752dfb3feb154
khaleel raweh <khaleelvisa@gmail.com>
2024-05-26 22:05:29 +0300

    save all

--- app/Http/Livewire/Frontend/Customer/StudentLessonSingleComponent copy.php
+++ app/Http/Livewire/Frontend/Customer/StudentLessonSingleComponent copy.php
@@ -11,20 +11,17 @@ class StudentLessonSingleComponent extends Component
 
     public $videoUrl;
 
-
-    public function mount()
-    {
-        $course = Course::with('sections.lessons')->where('slug->' . app()->getLocale(), $this->slug)->first();
-        $url = $course->sections->first()->lessons->first()->url;
-
-        if (empty($this->videoUrl)) {
-            $this->videoUrl = $url;
-        }
-    }
-
     public function render()
     {
         $course = Course::with('sections.lessons')->where('slug->' . app()->getLocale(), $this->slug)->first();
+
+        // Trim the text to remove leading and trailing spaces
+        $course->description = trim($course->description);
+        // Get the first 200 characters
+        $exposedText = substr($course->description, 0, 200);
+        // Get the rest of the text
+        $hiddenText = substr($course->description, 200);
+
         // Calculate total duration for each section
         $totalDurations = [];
         foreach ($course->sections as $section) {
@@ -32,7 +29,7 @@ class StudentLessonSingleComponent extends Component
             $totalDurations[$section->id] = $totalDuration;
         }
 
-        return view('livewire.frontend.customer.student-lesson-single-component', compact('course', 'totalDurations'));
+        return view('livewire.frontend.customer.student-lesson-single-component', compact('course', 'totalDurations', 'exposedText', 'hiddenText'));
     }
 
     public function updateVideoUrl($url)

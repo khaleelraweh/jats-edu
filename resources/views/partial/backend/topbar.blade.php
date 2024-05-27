87c6d3b468dea011cca526c4eb91c679f365f400
khaleel raweh <khaleelvisa@gmail.com>
2024-05-27 17:37:04 +0300

    save all

--- resources/views/partial/backend/topbar.blade.php
+++ resources/views/partial/backend/topbar.blade.php
@@ -36,7 +36,7 @@
             <!-- App Search-->
             <form class="app-search d-none d-lg-block">
                 <div class="position-relative">
-                    <input type="text" class="form-control" placeholder="Search...">
+                    <input type="text" class="form-control" placeholder="{{ __('panel.search') }}...">
                     <span class="ri-search-line"></span>
                 </div>
             </form>

d783ad558232d30ac822b2d833e07018dbb55903
khaleel raweh <khaleelvisa@gmail.com>
2024-05-27 19:00:48 +0300

    save all

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -91,7 +91,7 @@ class CourseSeeder extends Seeder
             //     'course_category_id' => 13, // Random category
             // ],
             [
-                'title' => ['ar' => 'دورة في الصوت والمسيقي', 'en' => 'A course in voice and music'],
+                'title' => ['ar' => 'HTML and CSS Template 4', 'en' => 'HTML and CSS Template 4'],
                 'course_category_id' => $categories->random(), // Random category
             ],
 
@@ -121,8 +121,8 @@ class CourseSeeder extends Seeder
                 'language' => 1,
                 'lecture_numbers' => 5,
                 'course_duration' => "8h 12m",
-                'video_promo' => "https://www.youtube.com/watch?v=9vn_e_XPV4s&list=PL_hXcCj5NytUlkH1XgfsjHAhJ0QHXM_xO",
-                'video_description' => "https://www.youtube.com/embed/1bTRzuWzX8Y?si=6WjMubVTEdD8Ow1o",
+                'video_promo' => "https://www.youtube.com/embed/4OGWPn-Q__I?si=Rrw3o9boTvacjX4M",
+                'video_description' => "https://www.youtube.com/embed/4OGWPn-Q__I?si=Rrw3o9boTvacjX4M",
                 'course_type' => 1,
                 'deadline' => "2024-03-11",
                 'certificate' => 1,

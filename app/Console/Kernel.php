59ed2580bffd2a99cce1799e1d46a947f26e3b14
khaleel raweh <khaleelvisa@gmail.com>
2024-06-04 18:54:44 +0300

    save all

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,16 @@
 
 namespace App\Console;
 
+use App\Console\Commands\DeleteReadNotifications;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    protected $commands = [
+        DeleteReadNotifications::class
+    ];
     /**
      * Define the application's command schedule.
      *
@@ -16,6 +21,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        $schedule->command('read-notifications:delete')->daily();
     }
 
     /**
@@ -25,7 +32,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }

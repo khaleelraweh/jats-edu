ea128658f4b01cd9d720379b4387b59e8efe9f39
khaleel raweh <khaleelvisa@gmail.com>
2024-06-05 00:32:43 +0300

    save all

--- app/Http/Livewire/Frontend/Header/AccountNotificationsComponent.php
+++ app/Http/Livewire/Frontend/Header/AccountNotificationsComponent.php
@@ -6,6 +6,32 @@ use Livewire\Component;
 
 class AccountNotificationsComponent extends Component
 {
+
+    public $unreadNotificationsCount = '';
+    public $unreadNotifications;
+
+    // to refresh the notification count in realtime 
+    public function getListeners(): array
+    {
+        $userId = auth()->id();
+        return [
+            "echo-notification:App.Models.User.{$userId},notification" => 'mount'
+        ];
+    }
+
+    public function mount()
+    {
+        $this->unreadNotificationsCount = auth()->user()->unreadNotifications->count();
+        $this->unreadNotifications = auth()->user()->unreadNotifications;
+    }
+
+    // to mark notification to readed on click on it 
+    public function markAsRead($id)
+    {
+        $notification = auth()->user()->unreadNotifications->where('id', $id)->first();
+        $notification->markAsRead();
+        return redirect()->to($notification->data['order_url']);
+    }
     public function render()
     {
         return view('livewire.frontend.header.account-notifications-component');
--- resources/views/livewire/frontend/header/account-notifications-component.blade.php
+++ resources/views/livewire/frontend/header/account-notifications-component.blade.php
@@ -6,7 +6,7 @@
             aria-expanded="false">
             <i class="ri-notification-3-line"></i>
             <span class="badge badge-coral text-white rounded-circle fw-bold badge-float mt-n1 ms-n2 px-0 w-16"
-                style="font-size: 8px;">2</span>
+                style="font-size: 8px;">{{ $unreadNotificationsCount }}</span>
         </a>
 
         <div class="dropdown-menu border-xl shadow-none dropdown-menu-md dropdown-menu-end p-0"
@@ -22,8 +22,35 @@
                 </div>
             </div>
             <div data-simplebar style="max-height: 230px;">
+                @forelse ($unreadNotifications as $unreadNotification)
+                    <a wire:click="markAsRead('{{ $unreadNotification->id }}')" class="text-reset notification-item"
+                        style="cursor: pointer">
+                        <div class="d-flex">
+                            <div class="avatar-xs me-3">
+                                <span class="avatar-title bg-primary rounded-circle font-size-16">
+                                    <i class="ri-shopping-cart-line"></i>
+                                </span>
+                            </div>
+                            <div class="flex-1">
+                                <h6 class="mb-1">{{ __('panel.from_customer') }}
+                                    {{ $unreadNotification->data['customer_name'] }}</h6>
+                                <div class="font-size-12 text-muted">
+                                    <p class="mb-1">{{ __('panel.a_new_order_with_amount') }}
+                                        ({{ currency_converter($unreadNotification->data['amount']) }})
+                                        {{ __('panel.from_customer') }}
+                                        ({{ $unreadNotification->data['customer_name'] }})
+                                    </p>
+                                    {{-- <p class="mb-0"><i class="mdi mdi-clock-outline"></i> 3 min ago</p> --}}
+                                    <p class="mb-0"><i class="mdi mdi-clock-outline"></i>
+                                        {{ $unreadNotification->data['created_date'] }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                @empty
+                    <div class="dropdown-item text-center">No notification found</div>
+                @endforelse
 
-                <div class="dropdown-item text-center">No notification found</div>
             </div>
             <div class="p-2 border-top">
                 <div class="d-grid">

67ec0788397bf910329ab637dfe727ac45254587
khaleel raweh <khaleelvisa@gmail.com>
2024-06-04 17:09:08 +0300

    added notification icon to backend theme

--- resources/views/partial/backend/topbar.blade.php
+++ resources/views/partial/backend/topbar.blade.php
@@ -48,45 +48,6 @@
         <div class="d-flex">
 
 
-            {{-- <div class="dropdown d-inline-block  ms-2">
-
-                <a href="{{ route('frontend.index') }}"
-                    class=" d-flex align-items-center btn btn-sm px-3 font-size-24 header-item waves-effect"
-                    id="vertical-menu-btn" title="واجهة العملاء">
-                    <i class="ri-home-wifi-line"></i>
-                </a>
-
-            </div> --}}
-
-            {{-- <div class="dropdown d-inline-block  ms-2">
-
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item dropdown">
-                        <a href="#"
-                            class="nav-link dropdown-toggle d-flex align-items-center btn btn-sm px-3 font-size-16 header-item waves-effect"
-                            id="languagesDropdown" data-toggle="dropdown" role="button" aria-expanded="false"
-                            aria-haspopup="true">
-                            {{ __('panel.' . config('locales.languages')[app()->getLocale()]['lang']) }}
-                            <span class="caret"></span>
-                            <i class="mdi mdi-chevron-down"></i>
-                        </a>
-
-                        <div class="dropdown-menu" aria-labelledby="languagesDropdown">
-                            @foreach (config('locales.languages') as $key => $val)
-                                @if ($key != app()->getLocale())
-                                    <a href="{{ route('change.language', $key) }}" class="dropdown-item">
-                                        {{ __('panel.' . $val['lang']) }}
-                                    </a>
-                                @endif
-                            @endforeach
-                        </div>
-
-                    </li>
-                </ul>
-
-            </div> --}}
-
-
 
             <div class="dropdown d-none d-sm-inline-block">
                 <button type="button" class="btn header-item waves-effect" data-bs-toggle="dropdown"
@@ -117,6 +78,96 @@
                 <button type="button" class="btn header-item noti-icon waves-effect" data-toggle="fullscreen">
                     <i class="ri-fullscreen-line"></i>
                 </button>
+            </div>
+
+
+            <div class="dropdown d-inline-block">
+                <button type="button" class="btn header-item noti-icon waves-effect"
+                    id="page-header-notifications-dropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="ri-notification-3-line"></i>
+                    <span class="noti-dot"></span>
+                </button>
+                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0"
+                    aria-labelledby="page-header-notifications-dropdown">
+                    <div class="p-3">
+                        <div class="row align-items-center">
+                            <div class="col">
+                                <h6 class="m-0"> Notifications </h6>
+                            </div>
+                            <div class="col-auto">
+                                <a href="#!" class="small"> View All</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div data-simplebar style="max-height: 230px;">
+                        <a href="" class="text-reset notification-item">
+                            <div class="d-flex">
+                                <div class="avatar-xs me-3">
+                                    <span class="avatar-title bg-primary rounded-circle font-size-16">
+                                        <i class="ri-shopping-cart-line"></i>
+                                    </span>
+                                </div>
+                                <div class="flex-1">
+                                    <h6 class="mb-1">Your order is placed</h6>
+                                    <div class="font-size-12 text-muted">
+                                        <p class="mb-1">If several languages coalesce the grammar</p>
+                                        <p class="mb-0"><i class="mdi mdi-clock-outline"></i> 3 min ago</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="" class="text-reset notification-item">
+                            <div class="d-flex">
+                                <img src="assets/images/users/avatar-3.jpg" class="me-3 rounded-circle avatar-xs"
+                                    alt="user-pic">
+                                <div class="flex-1">
+                                    <h6 class="mb-1">James Lemire</h6>
+                                    <div class="font-size-12 text-muted">
+                                        <p class="mb-1">It will seem like simplified English.</p>
+                                        <p class="mb-0"><i class="mdi mdi-clock-outline"></i> 1 hours ago</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="" class="text-reset notification-item">
+                            <div class="d-flex">
+                                <div class="avatar-xs me-3">
+                                    <span class="avatar-title bg-success rounded-circle font-size-16">
+                                        <i class="ri-checkbox-circle-line"></i>
+                                    </span>
+                                </div>
+                                <div class="flex-1">
+                                    <h6 class="mb-1">Your item is shipped</h6>
+                                    <div class="font-size-12 text-muted">
+                                        <p class="mb-1">If several languages coalesce the grammar</p>
+                                        <p class="mb-0"><i class="mdi mdi-clock-outline"></i> 3 min ago</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+
+                        <a href="" class="text-reset notification-item">
+                            <div class="d-flex">
+                                <img src="assets/images/users/avatar-4.jpg" class="me-3 rounded-circle avatar-xs"
+                                    alt="user-pic">
+                                <div class="flex-1">
+                                    <h6 class="mb-1">Salena Layfield</h6>
+                                    <div class="font-size-12 text-muted">
+                                        <p class="mb-1">As a skeptical Cambridge friend of mine occidental.</p>
+                                        <p class="mb-0"><i class="mdi mdi-clock-outline"></i> 1 hours ago</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="p-2 border-top">
+                        <div class="d-grid">
+                            <a class="btn btn-sm btn-link font-size-14 text-center" href="javascript:void(0)">
+                                <i class="mdi mdi-arrow-right-circle me-1"></i> View More..
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
 
 

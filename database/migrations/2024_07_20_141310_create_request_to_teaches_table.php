033b028b33710501c446b9bd83e745779e664500
khaleel raweh <khaleelvisa@gmail.com>
2024-07-20 17:33:39 +0300

    save all

---
+++ database/migrations/2024_07_20_141310_create_request_to_teaches_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request_to_teaches', function (Blueprint $table) {
+            $table->id();
+            $table->json('full_name');
+            $table->dateTime('date_of_birth')->nullable();
+            $table->string('place_of_birth')->nullable();
+            $table->string('nationality')->nullable();
+            $table->string('educational_qualification')->nullable();
+            $table->string('specialization')->nullable();
+            $table->unsignedTinyInteger('years_of_training_experience')->default(0);
+            $table->string('identity')->nullable(); // الهوية
+            $table->string('biography')->nullable(); // السيرة الذاتية
+            $table->string('motivation')->nullable(); // الحافز للتدريب في خطوة شباب 
+
+
+            // start will be use always
+            $table->boolean('status')->default(true);
+            $table->dateTime('published_on')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->string('deleted_by')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+            // end will be use always
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('request_to_teaches');
+    }
+};

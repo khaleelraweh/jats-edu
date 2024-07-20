95e479357e7733b8a6923a427752cfb07187e411
khaleel raweh <khaleelvisa@gmail.com>
2024-07-20 17:03:25 +0300

    save all

--- resources/views/frontend/customer/teach-on-jats-request.blade.php
+++ resources/views/frontend/customer/teach-on-jats-request.blade.php
@@ -27,86 +27,73 @@
             <div class="card">
                 <div class="card-body">
                     <div class="main-content-label mg-b-5">
-                        Basic Content Wizard
+                        إستمارة طلب تقديم كمدرب
                     </div>
-                    <p class="mg-b-20">It is Very Easy to Customize and it uses in your website apllication.</p>
+                    <p class="mg-b-20">ملاحظة : سيتم اعتماد البيانات عند الانتهاء من تعبئة البيانات وارسالها , لذلك نرجو
+                        القيام بإدخال كافة البيانات المطلوبة بشكل صحيح ودقيق.</p>
                     <div id="wizard1">
-                        <h3>Personal Information</h3>
+                        <h3>البيانات الشخصية</h3>
                         <section>
                             <div class="control-group form-group">
-                                <label class="form-label">Name</label>
-                                <input type="text" class="form-control required" placeholder="Name">
+                                <label class="form-label">الإسم باللغة العربية</label>
+                                <input type="text" class="form-control required" placeholder="الاسم بالعربي">
                             </div>
                             <div class="control-group form-group">
-                                <label class="form-label">Email</label>
-                                <input type="email" class="form-control required" placeholder="Email Address">
+                                <label class="form-label">الإسم باللغة الانجليزية</label>
+                                <input type="text" class="form-control required" placeholder="الاسم بالانجليزي">
                             </div>
                             <div class="control-group form-group">
-                                <label class="form-label">Phone Number</label>
-                                <input type="number" class="form-control required" placeholder="Number">
+                                <label class="form-label">تاريخ الميلاد</label>
+                                <input type="text" class="form-control required" placeholder="تاريخ الميلاد">
                             </div>
                             <div class="control-group form-group mb-0">
-                                <label class="form-label">Address</label>
-                                <input type="text" class="form-control required" placeholder="Address">
+                                <label class="form-label">محل الميلاد</label>
+                                <input type="text" class="form-control required" placeholder="محل الميلاد">
+                            </div>
+                            <div class="control-group form-group mb-0">
+                                <label class="form-label"> الجنسية</label>
+                                <input type="text" class="form-control required" placeholder="الجنسية">
+                            </div>
+                            <div class="control-group form-group mb-0">
+                                <label class="form-label"> المؤهل الدراسي</label>
+                                <input type="text" class="form-control required" placeholder="المؤهل الدراسي">
+                            </div>
+                            <div class="control-group form-group mb-0">
+                                <label class="form-label">التخصص</label>
+                                <input type="text" class="form-control required" placeholder="التخصص">
+                            </div>
+                            <div class="control-group form-group mb-0">
+                                <label class="form-label">سنوات خبرة التدريب</label>
+                                <input type="text" class="form-control required" placeholder="سنوات خبرة التدريب">
                             </div>
                         </section>
-                        <h3>Billing Information</h3>
+                        <h3>المرفقات</h3>
                         <section>
-                            <div class="table-responsive mg-t-20">
-                                <table class="table table-bordered">
-                                    <tbody>
-                                        <tr>
-                                            <td>Cart Subtotal</td>
-                                            <td class="text-right">$792.00</td>
-                                        </tr>
-                                        <tr>
-                                            <td><span>Totals</span></td>
-                                            <td class="text-right text-muted"><span>$792.00</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td><span>Order Total</span></td>
-                                            <td>
-                                                <h2 class="price text-right mb-0">$792.00</h2>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                            <div class="control-group form-group">
+                                <label class="form-label">ارفاق صورة من الهوية / جواز السفر (واضح)</label>
+                                <input type="text" class="form-control required" placeholder="الهوية او جواز السفر">
+                            </div>
+                            <div class="control-group form-group">
+                                <label class="form-label">السيرة الذاتية</label>
+                                <input type="text" class="form-control required" placeholder="السيرة الذاتية">
+                            </div>
+                            <div class="control-group form-group mb-0">
+                                <label class="form-label"> ارفاق الشهادات </label>
+                                <input type="text" class="form-control required" placeholder="الشهادات">
                             </div>
                         </section>
-                        <h3>Payment Details</h3>
+                        <h3>الحافز</h3>
                         <section>
                             <div class="form-group">
-                                <label class="form-label">CardHolder Name</label>
-                                <input type="text" class="form-control" id="name1" placeholder="First Name">
+                                <label class="form-label">لماذا تريد الالتحاق بالتدريب لدينا</label>
+                                <textarea class="form-control" name="" id="" cols="30" rows="10"></textarea>
                             </div>
-                            <div class="form-group">
-                                <label class="form-label">Card number</label>
-                                <div class="input-group">
-                                    <input type="text" class="form-control" placeholder="Search for...">
-                                    <span class="input-group-append">
-                                        <button class="btn btn-info" type="button"><i class="fab fa-cc-visa"></i> &nbsp;
-                                            <i class="fab fa-cc-amex"></i> &nbsp;
-                                            <i class="fab fa-cc-mastercard"></i></button>
-                                    </span>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-sm-8">
-                                    <div class="form-group mb-sm-0">
-                                        <label class="form-label">Expiration</label>
-                                        <div class="input-group">
-                                            <input type="number" class="form-control" placeholder="MM" name="expiremonth">
-                                            <input type="number" class="form-control" placeholder="YY" name="expireyear">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-4 ">
-                                    <div class="form-group mb-0">
-                                        <label class="form-label">CVV <i class="fa fa-question-circle"></i></label>
-                                        <input type="number" class="form-control" required="">
-                                    </div>
-                                </div>
-                            </div>
+
+                        </section>
+
+                        <h3>بيانات الاستمارة </h3>
+                        <section>
+                            يتم تصميم الخصلاصة هنا
                         </section>
                     </div>
                 </div>

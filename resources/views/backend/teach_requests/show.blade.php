f230e6dce45f4606b9ea1ddc092f6771d328e405
khaleel raweh <khaleelvisa@gmail.com>
2024-07-23 23:59:15 +0300

    save all

--- resources/views/backend/teach_requests/show.blade.php
+++ resources/views/backend/teach_requests/show.blade.php
@@ -59,9 +59,9 @@
         </div>
 
 
-        <div class="outer bg-white p-5">
+        <div class="outer bg-white p-4">
 
-            <div class="inner" style="border: 3px solid red ;">
+            <div class="inner" style="border: 1px solid #777777 ;">
                 <header>
                     <div class="row">
                         <div class="col-sm-10 offset-1 bg-white">
@@ -83,7 +83,7 @@
                     </div>
 
                     <div class="row">
-                        <div class="col-sm-12 ">
+                        <div class="col-sm-12 pt-3 ">
                             <hr>
                         </div>
                     </div>
@@ -91,10 +91,10 @@
                 </header>
 
                 <div class="section  px-5 pb-3">
-                    <div class="row border">
+                    <div class="row" style="border: 6px double green;">
                         <div class="col-sm-10 offset-1 bg-white">
                             <div class="row">
-                                <div class="col-4">
+                                <div class="col-4 ">
                                     <span>تاريخ الطلب : 5/5/2024</span>
                                 </div>
                                 <div class="col-4">
@@ -107,61 +107,69 @@
                             </div>
 
                             <div class="row">
-                                <div class="col-12">
-                                    <span class="arabic-name">الاسم الكامل باللغة العربية : </span>
+                                <div class="col-sm-12 pt-3  ">
+
+                                    <span class="arabic-name"> الإسم الكامل (باللغة العربية ) : </span>
                                     <span class="arabic-name-value">وليد</span>
+
+
                                 </div>
                             </div>
 
                             <div class="row">
-                                <div class="col-12">
-                                    <span class="english-name"> Full Name (باللغة الانجليزية):</span>
-                                    <span class="english-name-value">waled blua blau </span>
+                                <div class="col-sm-12 pt-3 text-end">
+                                    <bdo dir="ltr">
+                                        <span class="english-name"> Full Name (In English) : </span>
+                                        <span class="english-name-value">waled blua blau </span>
+                                    </bdo>
+
                                 </div>
                             </div>
 
                             <div class="row">
-                                <div class="col-sm-12 col-md-4">
+                                <div class="col-sm-12 pt-3 col-md-4">
                                     <span class="date-of-birth">تاريخ الميلاد : </span>
                                     <span class="date-of-birth-value"> 2000/2/2</span>
                                 </div>
-                                <div class="col-sm-12 col-md-4">
+                                <div class="col-sm-12 pt-3 col-md-4">
                                     <span class="place-of-birth">محل الميلاد : </span>
                                     <span class="place-of-birth-value">Ibb</span>
                                 </div>
-                                <div class="col-sm-12 col-md-4">
+                                <div class="col-sm-12 pt-3 col-md-4">
                                     <span class="nationality">الجنسية</span>
                                     <span class="nationality-value">يمني</span>
                                 </div>
                             </div>
 
                             <div class="row">
-                                <div class="col-sm-12 col-md-6">
+                                <div class="col-sm-12 pt-3 col-md-4">
                                     <span class="residence-address">عنوان الإقامة \ السكن : </span>
                                     <span class="residence-address-value">دار القدسي</span>
                                 </div>
-                                <div class="col-sm-12 col-md-6">
+
+                                <div class="col-sm-12 pt-3 col-md-4">
                                     <span class="phone"> رقم الجوال :</span>
                                     <span class="phone-value"> 772036131 </span>
                                 </div>
+                                <div class="col-sm-12 col-md-4"></div>
                             </div>
                             <div class="row">
-                                <div class="col-sm-12 col-md-4">
+                                <div class="col-sm-12 pt-3 col-md-4">
                                     <span class="educational_qualification"> المؤهل الدراسي : </span>
                                     <span class="educational-qualification-value">بكلوريوس</span>
                                 </div>
-                                <div class="col-sm-12 col-md-4">
+                                <div class="col-sm-12 pt-3 col-md-4">
                                     <span class="specialization">التخصص : </span>
                                     <span class="specialization-value"> علوم حاسوب </span>
                                 </div>
-                                <div class="col-sm-12 col-md-4">
+                                <div class="col-sm-12 pt-3 col-md-4">
                                     <span class="years_of_training_experience"> سنوات خبرة التدريب : </span>
                                     <span class="years_of_training_experience-value"> 4 </span>
                                 </div>
                             </div>
 
                             <div class="row">
-                                <div class="col-sm-12">
+                                <div class="col-sm-12 pt-3">
                                     <div class="motivation">لماذا تريد الإلتحاق بالتدريب لدينا : </div>
                                     <div class="motivation-value">
                                         Lorem ipsum dolor, sit amet consectetur adipisicing elit.
@@ -173,15 +181,15 @@
                             </div>
 
                             <div class="row">
-                                <div class="col-sm-12 col-md-4">
+                                <div class="col-sm-12 pt-3 col-md-4">
                                     <span class="identity"> صورة من الهوية / جواز السفر : </span>
                                     <span class="identity-value"> <a href="#">identity-link</a> </span>
                                 </div>
-                                <div class="col-sm-12 col-md-4">
+                                <div class="col-sm-12 pt-3 col-md-4">
                                     <span class="biography"> إرفاق السيرة الذاتية : </span>
                                     <span class="biography-value"><a href="#">biography-link</a></span>
                                 </div>
-                                <div class="col-sm-12 col-md-4">
+                                <div class="col-sm-12 pt-3 col-md-4">
                                     <span class="Certificates"> إرفاق الشهادات : </span>
                                     <span class="Certificates-value"><a href="#">Certificates-link</a></span>
                                 </div>
@@ -189,7 +197,7 @@
 
 
                             <div class="row">
-                                <div class="col-sm-12">
+                                <div class="col-sm-12 pt-3">
                                     <p>
                                         ملاحظة : سيتم اعتماد البيانات أعلاه عند الارسال ،
                                         لذا نرجو القيام

2333209cfec9ee19ff2d51156d9e3c12091eabc4
khaleel raweh <khaleelvisa@gmail.com>
2024-08-08 22:07:42 +0300

    save all

--- resources/views/backend/teach_requests/show.blade.php
+++ resources/views/backend/teach_requests/show.blade.php
@@ -283,7 +283,8 @@
                                     </div>
                                     <div class="col-sm-12 pt-3 col-md-4">
                                         <span class="specialization ww ">التخصص : </span>
-                                        <span class="specialization-value"> {{ $teach_request->specialization }} </span>
+                                        <span class="specialization-value"> {{ $teach_request->specialization->name }}
+                                        </span>
                                     </div>
                                     <div class="col-sm-12 pt-3 col-md-4">
                                         <span class="years_of_training_experience ww">

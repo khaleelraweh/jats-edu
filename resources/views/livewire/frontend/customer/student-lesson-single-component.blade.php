f16ca637f7acadd53ccc63894b9cd99e49b3d7ee
khaleel raweh <khaleelvisa@gmail.com>
2024-08-19 01:56:00 +0300

    save all

--- resources/views/livewire/frontend/customer/student-lesson-single-component.blade.php
+++ resources/views/livewire/frontend/customer/student-lesson-single-component.blade.php
@@ -42,7 +42,15 @@
                         <h3 class="text-white">{{ $selectedEvaluation->title }}</h3>
                         <p class="text-white">{{ $selectedEvaluation->description }}</p>
 
-
+                        @foreach ($selectedEvaluation->questions as $question)
+                            {{ $question->question_text }}
+                            <br>
+                            @foreach ($question->options as $option)
+                                <input type="{{ $question->question_type == 0 ? 'radio' : 'checkbox' }}">
+                                {{ $option->option_text }} <br>
+                            @endforeach
+                            <br><br>
+                        @endforeach
 
 
                     </div>

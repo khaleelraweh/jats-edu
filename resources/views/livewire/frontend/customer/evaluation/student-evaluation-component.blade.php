9b9fbba089ec3cfbfd5e195d16504feaa347b827
khaleel raweh <khaleelvisa@gmail.com>
2024-08-19 18:30:02 +0300

    save all

--- resources/views/livewire/frontend/customer/evaluation/student-evaluation-component.blade.php
+++ resources/views/livewire/frontend/customer/evaluation/student-evaluation-component.blade.php
@@ -26,22 +26,6 @@
 
     <h3 class="text-white">Evaluation Container </h3>
 
-
-    {{-- @foreach ($selectedEvaluation->questions as $question)
-        {{ $question->question_text }}
-        <br>
-        @foreach ($question->options as $option)
-            <input type="{{ $question->question_type == 0 ? 'radio' : 'checkbox' }}">
-            {{ $option->option_text }} <br>
-        @endforeach
-        <br><br>
-    @endforeach  --}}
-
-
-
-
-    {{-- ============================== start ========================== --}}
-
     <div class="card">
         <div class="card-header" style="background: rgb(47,45,81,0.8);color:white;">
             <div class="card-title">
@@ -81,37 +65,6 @@
                 <!------------- part 2 : Content ------------->
                 <div class="mycontent">
 
-
-                    {{-- <h3 id="wizard1-h-0" tabindex="-1" class="title {{ $currentStep == 1 ? 'current' : '' }} ">
-                        {{ __('panel.document_template_data') }}
-                    </h3>
-
-                    <section id="wizard1-p-0" role="tabpanel" aria-labelledby="wizard1-h-0"
-                        class="body {{ $currentStep == 1 ? 'current' : '' }}  step"
-                        aria-hidden="{{ $currentStep == 1 ? 'false' : 'true' }}"
-                        style="display: {{ $currentStep == 1 ? 'block' : 'none' }}">
-
-                        <form method="post">
-                            @csrf
-                            @method('PUT')
-
-                            <div class="row">
-                                <div class="col-sm-12 col-md-12">
-                                    <div class="row">
-                                        welcome
-                                    </div>
-
-
-
-                                </div>
-
-
-                            </div>
-                        </form>
-                    </section> --}}
-
-
-
                     <!---- related to dynamic steps --->
                     @isset($questionData)
                         @foreach ($questionData['questions'] as $questionIndex => $question)
@@ -120,7 +73,7 @@
 
                                 <div class="row align-items-end mb-4 mb-md-0">
                                     <div class="col-md mb-4 mb-md-0">
-                                        {{-- <h4>{{ $question['question_text'] }}</h4> --}}
+                                        <h4>{{ $question['question_text'] }}</h4>
                                     </div>
                                     <div class="col-md-auto aos-init aos-animate" data-aos="fade-start">
                                     </div>
@@ -136,25 +89,25 @@
                                     <div class="row">
                                         <div class="col-sm-12 col-md-12">
 
-                                            <fieldset>
-                                                <legend>{{ $question['question_text'] }}</legend>
+                                            {{-- <fieldset> --}}
+                                            {{-- <legend>{{ $question['question_text'] }}</legend> --}}
 
 
-                                                <div class="row">
-                                                    @foreach ($question['options'] as $optionIndex => $option)
-                                                        <div class="col">
+                                            <div class="row">
+                                                @foreach ($question['options'] as $optionIndex => $option)
+                                                    <div class="col">
 
-                                                            <input
-                                                                type="{{ $question['question_type'] == 0 ? 'radio' : 'checkbox' }}"
-                                                                wire:model.defer="questionData.{{ $questionIndex }}.options.{{ $optionIndex }}.option_value"
-                                                                value="{{ $option['option_id'] }}">
-                                                            {{ $option['option_text'] }}
-                                                        </div>
-                                                    @endforeach
-                                                </div>
+                                                        <input
+                                                            type="{{ $question['question_type'] == 0 ? 'radio' : 'checkbox' }}"
+                                                            wire:model.defer="questionData.{{ $questionIndex }}.options.{{ $optionIndex }}.option_value"
+                                                            value="{{ $option['option_id'] }}">
+                                                        {{ $option['option_text'] }}
+                                                    </div>
+                                                @endforeach
+                                            </div>
 
 
-                                            </fieldset>
+                                            {{-- </fieldset> --}}
 
                                         </div>
                                     </div>

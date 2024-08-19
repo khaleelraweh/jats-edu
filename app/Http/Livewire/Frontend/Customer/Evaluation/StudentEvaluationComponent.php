034a89f103e3bccf8b12e84669207cd022f4d534
khaleel raweh <khaleelvisa@gmail.com>
2024-08-19 16:54:07 +0300

    save all

--- app/Http/Livewire/Frontend/Customer/Evaluation/StudentEvaluationComponent.php
+++ app/Http/Livewire/Frontend/Customer/Evaluation/StudentEvaluationComponent.php
@@ -18,25 +18,53 @@ class StudentEvaluationComponent extends Component
     public $selectedEvaluation;
     public $questionCount;
 
+    public $questionData = [];
+
     protected $listeners = ['evaluationChanged' => 'reloadData'];
 
-
-    public function  mount($selectedEvaluation)
+    public function mount($selectedEvaluation)
     {
         $this->selectedEvaluation = $selectedEvaluation;
+
+        if ($this->selectedEvaluation) {
+            // Populate the questionData array
+            $this->questionData = [
+                'evaluation_id'                 => $this->selectedEvaluation->id,
+                'evaluation_title'              => $this->selectedEvaluation->title,
+                'evaluation_description'        => $this->selectedEvaluation->description,
+                'evaluation_course_section_id'  => $this->selectedEvaluation->course_section_id,
+
+                'questions' => $this->selectedEvaluation->questions->map(function ($question) {
+                    return [
+                        'question_id'               =>  $question->id,
+                        'question_text'             => $question->question_text,
+                        'question_type'             => $question->question_type,
+                        'question_evaluation_id'    => $question->evaluation_id,
+
+                        'options' => $question->options->map(function ($option) {
+                            return [
+                                'option_id'             => $option->id,
+                                'option_text'           => $option->option_text,
+                                'is_correct'            => $option->is_correct,
+                                'option_question_id'    => $option->question_id,
+                            ];
+                        })->toArray(),
+                    ];
+                })->toArray(),
+            ];
+
+            // Set the question count and total steps
+            $this->questionCount = count($this->questionData['questions']);
+            $this->totalSteps = $this->questionCount;
+        }
     }
+
 
     public function reloadData($selectedEvaluationId)
     {
         if ($selectedEvaluationId) {
-            $selectedEvaluation = Evaluation::find($selectedEvaluationId);
-            $this->mount($selectedEvaluation);
-        }
-
-        // Update the question count
-        if ($this->selectedEvaluation) {
-            $this->questionCount = $this->selectedEvaluation->questions()->count();
-            $this->totalSteps = $this->questionCount;
+            $this->selectedEvaluation = Evaluation::with('questions.options')->find($selectedEvaluationId);
+            $this->mount($this->selectedEvaluation);
         }
     }
 

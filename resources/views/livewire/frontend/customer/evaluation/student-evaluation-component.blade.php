00d9239189d61937245eb4cdedfe79a53ae1fc98
khaleel raweh <khaleelvisa@gmail.com>
2024-08-19 17:27:04 +0300

    save all

--- resources/views/livewire/frontend/customer/evaluation/student-evaluation-component.blade.php
+++ resources/views/livewire/frontend/customer/evaluation/student-evaluation-component.blade.php
@@ -1,6 +1,34 @@
 <div>
-
     <link rel="stylesheet" href="{{ asset('frontend/assets/css/mywizard.css') }}">
+
+    <style>
+        body {
+            font-family: 'DejaVu Sans', sans-serif;
+            direction: rtl;
+            text-align: right;
+        }
+
+        fieldset {
+            border: 1px solid #ddd;
+            border-radius: 4px;
+            margin-bottom: 20px;
+            padding: 15px;
+        }
+
+        legend {
+            font-size: 1.2em;
+            color: #333;
+            font-weight: bold;
+            width: fit-content;
+            padding: 0 0.7rem;
+        }
+
+        label {
+            display: block;
+            margin: 10px 0 5px;
+            color: #555;
+        }
+    </style>
 
     <h3 class="text-white">Evaluation Container </h3>
 
@@ -29,61 +57,61 @@
                 {{ $selectedEvaluation->description }}
             </div>
         </div>
-    </div>
+        <div class="card-body">
+            <div class="mywizard">
+                <!------------- part 1 : Steps ------------->
+                <div class="steps clearfix">
+                    <ul role="tablist">
+                        <li role="tab" wire:click="directMoveToStep(1)"
+                            class="first {{ $currentStep == 1 ? 'current' : '' }}" aria-disabled="false"
+                            aria-selected="true">
+                            <a id="wizard1-t-0" href="#wizard1-h-0" aria-controls="wizard1-p-0">
+                                <span class="current-info audible">current step:
+                                </span>
+                                <span class="number">1</span>
+                                <span class="title">
+                                    {{ __('panel.document_template_data') }}
+                                </span>
+                            </a>
+                        </li>
 
-    <div class="mywizard">
-        <!------------- part 1 : Steps ------------->
-        <div class="steps clearfix">
-            <ul role="tablist">
-                <li role="tab" wire:click="directMoveToStep(1)"
-                    class="first {{ $currentStep == 1 ? 'current' : '' }}" aria-disabled="false" aria-selected="true">
-                    <a id="wizard1-t-0" href="#wizard1-h-0" aria-controls="wizard1-p-0">
-                        <span class="current-info audible">current step:
-                        </span>
-                        <span class="number">1</span>
-                        <span class="title">
-                            {{ __('panel.document_template_data') }}
-                        </span>
-                    </a>
-                </li>
 
+                    </ul>
+                </div>
+                <!------------- part 1 : Steps end ------------->
 
-            </ul>
-        </div>
-        <!------------- part 1 : Steps end ------------->
+                <!------------- part 2 : Content ------------->
+                <div class="mycontent">
 
-        <!------------- part 2 : Content ------------->
-        <div class="mycontent">
+                    <!---- related to step 1 ----->
 
-            <!---- related to step 1 ----->
+                    <h3 id="wizard1-h-0" tabindex="-1" class="title {{ $currentStep == 1 ? 'current' : '' }} ">
+                        {{ __('panel.document_template_data') }}
+                    </h3>
 
-            <h3 id="wizard1-h-0" tabindex="-1" class="title {{ $currentStep == 1 ? 'current' : '' }} ">
-                {{ __('panel.document_template_data') }}
-            </h3>
+                    <section id="wizard1-p-0" role="tabpanel" aria-labelledby="wizard1-h-0"
+                        class="body {{ $currentStep == 1 ? 'current' : '' }}  step"
+                        aria-hidden="{{ $currentStep == 1 ? 'false' : 'true' }}"
+                        style="display: {{ $currentStep == 1 ? 'block' : 'none' }}">
 
-            <section id="wizard1-p-0" role="tabpanel" aria-labelledby="wizard1-h-0"
-                class="body {{ $currentStep == 1 ? 'current' : '' }}  step"
-                aria-hidden="{{ $currentStep == 1 ? 'false' : 'true' }}"
-                style="display: {{ $currentStep == 1 ? 'block' : 'none' }}">
+                        <form method="post">
+                            @csrf
+                            @method('PUT')
 
-                <form method="post">
-                    @csrf
-                    @method('PUT')
-
-                    <div class="row">
-                        <div class="col-sm-12 col-md-12">
                             <div class="row">
-                                welcome
+                                <div class="col-sm-12 col-md-12">
+                                    <div class="row">
+                                        welcome
+                                    </div>
+
+
+
+                                </div>
+
+
                             </div>
-
-
-
-                        </div>
-
-
-                    </div>
-                </form>
-            </section>
+                        </form>
+                    </section>
 
 
 
@@ -92,39 +120,44 @@
 
 
 
+                </div>
+                <!------------- part 2 : Content end ------------->
+
+                <!------------- part 2 : navagition wizard ------------->
+                <div class="actions clearfix">
+                    <ul role="menu" aria-label="Pagination">
+                        <li class="{{ $currentStep == 1 ? 'disabled' : '' }}"
+                            aria-disabled="{{ $currentStep == 1 ? 'true' : 'false' }}">
+                            <a href="#previous" style="display: {{ $currentStep == 1 ? 'none' : 'none' }} ;"
+                                role="menuitem">
+                                Previous
+                            </a>
+                            <a href="#previous" wire:click="previousStep"
+                                style="display: {{ $currentStep == 1 ? 'none' : 'block' }};" role="menuitem">
+                                {{ __('panel.previous') }}
+                            </a>
+                        </li>
+
+                        <li aria-hidden="false" aria-disabled="false"
+                            style="display: {{ $currentStep == $totalSteps ? 'none' : 'block' }}">
+                            <a href="#next" wire:click="nextStep" role="menuitem">
+                                التالي
+                            </a>
+                        </li>
+
+                        <li aria-hidden="true" style="display: {{ $currentStep == $totalSteps ? 'block' : 'none' }}">
+                            <a href="#finish" wire:click="finish" role="menuitem">
+                                {{ __('panel.finish') }}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <!------------- part 2 : navagition wizard end ------------->
+            </div>
         </div>
-        <!------------- part 2 : Content end ------------->
-
-        <!------------- part 2 : navagition wizard ------------->
-        <div class="actions clearfix">
-            <ul role="menu" aria-label="Pagination">
-                <li class="{{ $currentStep == 1 ? 'disabled' : '' }}"
-                    aria-disabled="{{ $currentStep == 1 ? 'true' : 'false' }}">
-                    <a href="#previous" style="display: {{ $currentStep == 1 ? 'none' : 'none' }} ;" role="menuitem">
-                        Previous
-                    </a>
-                    <a href="#previous" wire:click="previousStep"
-                        style="display: {{ $currentStep == 1 ? 'none' : 'block' }};" role="menuitem">
-                        {{ __('panel.previous') }}
-                    </a>
-                </li>
-
-                <li aria-hidden="false" aria-disabled="false"
-                    style="display: {{ $currentStep == $totalSteps ? 'none' : 'block' }}">
-                    <a href="#next" wire:click="nextStep" role="menuitem">
-                        التالي
-                    </a>
-                </li>
-
-                <li aria-hidden="true" style="display: {{ $currentStep == $totalSteps ? 'block' : 'none' }}">
-                    <a href="#finish" wire:click="finish" role="menuitem">
-                        {{ __('panel.finish') }}
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <!------------- part 2 : navagition wizard end ------------->
     </div>
+
+
 
 
 

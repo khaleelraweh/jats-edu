665ceee8e5effcc5694e485464069c727d7b84d1
khaleel raweh <khaleelvisa@gmail.com>
2024-08-21 17:21:42 +0300

    save all

--- resources/views/livewire/frontend/customer/student-lesson-single-component.blade.php
+++ resources/views/livewire/frontend/customer/student-lesson-single-component.blade.php
@@ -171,7 +171,6 @@
                                                             d="M12.8704 6.15374L3.42038 0.328572C2.73669 -0.0923355 1.9101 -0.109836 1.20919 0.281759C0.508282 0.673291 0.0898438 1.38645 0.0898438 2.18929V13.7866C0.0898438 15.0005 1.06797 15.9934 2.27016 16C2.27344 16 2.27672 16 2.27994 16C2.65563 16 3.04713 15.8822 3.41279 15.6591C3.70694 15.4796 3.79991 15.0957 3.62044 14.8016C3.44098 14.5074 3.05697 14.4144 2.76291 14.5939C2.59188 14.6982 2.42485 14.7522 2.27688 14.7522C1.82328 14.7497 1.33763 14.3611 1.33763 13.7866V2.18933C1.33763 1.84492 1.51713 1.53907 1.81775 1.3711C2.11841 1.20314 2.47294 1.21064 2.76585 1.39098L12.2159 7.21615C12.4999 7.39102 12.6625 7.68262 12.6618 8.01618C12.6611 8.34971 12.4974 8.64065 12.2118 8.81493L5.37935 12.9983C5.08548 13.1783 4.9931 13.5623 5.17304 13.8562C5.35295 14.1501 5.73704 14.2424 6.03092 14.0625L12.8625 9.87962C13.5166 9.48059 13.9081 8.78496 13.9096 8.01868C13.9112 7.25249 13.5226 6.55524 12.8704 6.15374Z"
                                                             fill="currentColor" />
                                                     </svg>
-
                                                 </a>
                                             </div>
                                         </div>
@@ -185,7 +184,6 @@
                                             <div class="d-flex align-items-center me-auto mb-4 mb-md-0">
                                                 <div class="text-secondary d-flex">
                                                     <i class="fas fa-file-alt"></i>
-
                                                 </div>
 
                                                 <div class="ms-4">
@@ -195,24 +193,49 @@
 
                                             <div
                                                 class="d-flex align-items-center overflow-auto overflow-md-visible flex-shrink-all">
-
                                                 {{-- evaluation director --}}
                                                 <a href="#"
                                                     wire:click="updateContent('{{ $evaluation->id }}', true)"
                                                     class="text-secondary d-flex">
                                                     <!-- Icon -->
                                                     <i class="fas fa-file"></i>
-
                                                 </a>
                                             </div>
                                         </div>
                                     @endforeach
-
-
-
                                 </div>
                             </div>
                         @endforeach
+
+                        <style>
+                            .certificate-links {
+                                border: 2px solid #2F2D51;
+                                border-radius: 5px;
+                            }
+
+                            .certificate-links:hover {
+                                background-color: #2F2D51;
+                            }
+                        </style>
+
+                        <a href="#"
+                            class="d-flex align-items-center p-5 mb-4 text-white fw-medium  line-height-one certificate-links">
+                            <span class="me-4 text-white d-flex">
+                                <!-- Icon -->
+                                <i class="fas fa-graduation-cap"></i>
+                            </span>
+                            الحصول على شهادة مشاركة
+                        </a>
+
+                        <a href="#" style="border: 2px solid #2F2D51;border-radius: 5px; "
+                            class="d-flex align-items-center p-5  text-white fw-medium  line-height-one certificate-links">
+                            <span class="me-4 text-white d-flex">
+                                <!-- Icon -->
+                                <i class="fas fa-user-graduate"></i>
+                            </span>
+                            الحصول على شهادة رسمية
+                        </a>
+
 
                     </div>
                 </div>

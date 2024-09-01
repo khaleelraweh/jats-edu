bd19f870404353bb23f4a6f25739cf9383be7bed
khaleel raweh <khaleelvisa@gmail.com>
2024-09-01 18:26:29 +0300

    save all

--- resources/views/frontend/customer/certification.blade.php
+++ resources/views/frontend/customer/certification.blade.php
@@ -1,5 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    we are in certification
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <div class="card-title">
+                    تاكيد بيانات الشهادة
+                </div>
+            </div>
+            <div class="card-body">
+                <form action="">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <label for="user_name">الإسم</label>
+                            <input type="text" class="form-control" value="{{ Auth::user()->full_name }}">
+                        </div>
+                    </div>
+
+
+                    <div class="row">
+                        <div class="col-sm-12 pt-3">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa totam cupiditate nesciunt eaque
+                            esse ut, dolorum optio est eligendi aspernatur aliquam eius odio minus quod illo ex accusantium
+                            dignissimos voluptate.
+                        </div>
+                    </div>
+
+
+                    <div class="form-group pt-3">
+                        <button type="submit" name="submit" class="btn btn-primary">
+                            {{ __('panel.save_data') }}</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection

0a45e76194e044ecf45c9e236eb722f91ef2228c
khaleel raweh <khaleelvisa@gmail.com>
2024-09-01 17:15:38 +0300

    save all

---
+++ database/migrations/2024_09_01_135930_create_certifications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('certifications', function (Blueprint $table) {
+            $table->id();
+            $table->string('cert_file'); // اسم الشهادة تكون برقم الاصدار
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->dateTime('date_of_issue')->nullable();
+            $table->integer('cert_code')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('certifications');
+    }
+};

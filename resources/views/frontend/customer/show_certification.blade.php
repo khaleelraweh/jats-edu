26aaf9ce4c48e260b003eb6e81fe089b927fe511
khaleel raweh <khaleelvisa@gmail.com>
2024-09-02 10:40:12 +0300

    save all

--- resources/views/frontend/customer/show_certification.blade.php
+++ resources/views/frontend/customer/show_certification.blade.php
@@ -10,6 +10,11 @@
             </div>
             <div class="card-body">
                 <div class="row">
+                    <div class="col-sm-12">
+                        <img src="{{ asset('assets/certifications/1.jpg') }}" alt="" style="width: 100%">
+                    </div>
+                </div>
+                <div class="row">
                     <div class="col-sm-12 text-center">
                         <a class="btn btn-primary">استعراض الشهادة</a class="btn btn-primary">
                         <a class="btn btn-primary">تنزيل الشهادة</a class="btn btn-primary">

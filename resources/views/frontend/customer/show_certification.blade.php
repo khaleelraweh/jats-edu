fb1c258cd16a9efd68d03a9a8f91c00ec70c53bb
khaleel raweh <khaleelvisa@gmail.com>
2024-09-02 11:13:03 +0300

    save all

--- resources/views/frontend/customer/show_certification.blade.php
+++ resources/views/frontend/customer/show_certification.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.app')
 
+@section('style')
+    <!-- Custom CSS -->
+    <style>
+        .modal-custom {
+            max-width: 60% !important;
+        }
+
+        @media (min-width: 992px) {
+            .modal-custom {
+                max-width: 50%;
+            }
+        }
+
+        .modal-image {
+            width: 100%;
+            height: auto;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        .modal-header {
+            display: none;
+        }
+
+        .modal-footer {
+            display: none;
+        }
+    </style>
+@endsection
+
 @section('content')
     <div class="container">
         <div class="card">
@@ -17,11 +48,49 @@
                 </div>
                 <div class="row">
                     <div class="col-sm-12 text-center">
-                        <a class="btn btn-primary">استعراض الشهادة</a class="btn btn-primary">
-                        <a class="btn btn-primary">تنزيل الشهادة</a class="btn btn-primary">
+                        {{-- <a class="btn btn-primary">استعراض الشهادة</a> --}}
+
+
+                        <a href="#" data-toggle="modal" data-target="#identityModal" class="btn btn-primary">
+                            استعراض الشهادة
+
+                        </a>
+
+
+                        <a class="btn btn-primary">تنزيل الشهادة</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <!-- Modal Identity image -->
+    <div class="modal fade" id="identityModal" tabindex="-1" role="dialog" aria-labelledby="identityModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-custom" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="identityModalLabel">Identity Image</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <img src="{{ asset('assets/certifications/' . $certificate->cert_file) }}" alt="Identity Image"
+                        class="img-fluid modal-image">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
+@section('script')
+    <!-- Include Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 @endsection

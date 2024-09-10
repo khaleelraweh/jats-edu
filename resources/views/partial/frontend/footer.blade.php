134b1601cb06a5293d2ae153cf7bdade8f382478
khaleel raweh <khaleelvisa@gmail.com>
2024-09-10 16:03:36 +0300

    save all

--- resources/views/partial/frontend/footer.blade.php
+++ resources/views/partial/frontend/footer.blade.php
@@ -36,7 +36,7 @@
 
     <div class="alert alert-primary alert-dismissible fade show my-wrapper" role="alert">
 
-        <a href="https://api.whatsapp.com" class="d-block text-white">
+        <a href="https://api.whatsapp.com/send?phone=967777282953&text=هل يمكنك المساعدة" class="d-block text-white">
             <div class="alert-body d-flex justify-content-between align-items-center">
 
                 <div class="part1">

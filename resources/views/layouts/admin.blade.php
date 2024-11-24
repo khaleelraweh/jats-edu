eef9b9015cb02ed11c37d55e1580354ae20d7aaf
khaleel raweh <khaleelvisa@gmail.com>
2024-11-24 15:15:50 +0300

    save all

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -51,15 +51,15 @@
         href="{{ asset('backend/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css') }}" />
     <link
         href="{{ asset('backend/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet"
-                                                                                                                                                                                                                                                                type="text/css') }}" />
+                                                                                                                                                                                                                                                                        type="text/css') }}" />
     <link
         href="{{ asset('backend/libs/datatables.net-select-bs4/css//select.bootstrap4.min.css" rel="stylesheet"
-                                                                                                                                                                                                                                                                type="text/css') }}" />
+                                                                                                                                                                                                                                                                        type="text/css') }}" />
 
     <!-- Responsive datatable examples -->
     <link
         href="{{ asset('backend/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet"
-                                                                                                                                                                                                                                                                type="text/css') }}" />
+                                                                                                                                                                                                                                                                        type="text/css') }}" />
 
     {{-- end  new  --}}
 
@@ -167,7 +167,7 @@
     <script src="{{ asset('backend/vendor/bootstrap-fileinput/js/fileinput.min.js') }}"></script>
     <script src="{{ asset('backend/vendor/bootstrap-fileinput/themes/fa5/theme.min.js') }}"></script>
     <!-- Datatable init js -->
-    <script src="{{ asset('backend/js/pages/dashboard.init.js') }}"></script>
+    {{-- <script src="{{ asset('backend/js/pages/dashboard.init.js') }}"></script> --}}
 
 
     <!-- Plugin js for Tinymce -->

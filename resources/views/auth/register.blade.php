1284d3409db86f84abaa58cfd011460bc93d9061
khaleel raweh <khaleelvisa@gmail.com>
2024-11-24 21:38:26 +0300

    save all

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,6 +5,12 @@
         .iti {
             width: 100%;
         }
+
+        /* Custom style for error messages */
+        .invalid-feedback {
+            font-size: 0.875rem;
+            /* Adjust font size for better responsiveness */
+        }
     </style>
 @endsection
 
@@ -26,124 +32,106 @@
             <div class="col-md-6 offset-md-3">
                 <h2 class="h5 text-uppercase mb-3">{{ __('transf.Create a new account') }}</h2>
 
-
-
                 <form method="POST" action="{{ route('register') }}">
                     @csrf
 
-
                     <div class="form-group mb-5">
-
                         <label for="first_name">
                             <i class="fa fa-user custom-color"></i>
                             {{ __('panel.first_name') }}
                             <span class="required">*</span>
                         </label>
-
-                        <input type="text" name="first_name" id="first_name" class="form-control  " value=""
-                            placeholder="{{ __('panel.first_name') }}">
+                        <input type="text" name="first_name" id="first_name" class="form-control"
+                            value="{{ old('first_name') }}" placeholder="{{ __('panel.first_name') }}">
 
                         @error('first_name')
-                            <span class="invalid-feedback text-danger" role="alert">
+                            <div class="invalid-feedback d-block">
                                 <strong>{{ $message }}</strong>
-                            </span>
+                            </div>
                         @enderror
-
                     </div>
 
                     <div class="form-group mb-5">
-
                         <label for="last_name">
                             <i class="fa fa-user custom-color"></i>
                             {{ __('panel.last_name') }}
                             <span class="required">*</span>
                         </label>
-
-                        <input type="text" name="last_name" id="last_name" class="form-control " value=""
-                            placeholder="{{ __('panel.last_name') }}">
+                        <input type="text" name="last_name" id="last_name" class="form-control"
+                            value="{{ old('last_name') }}" placeholder="{{ __('panel.last_name') }}">
 
                         @error('last_name')
-                            <span class="invalid-feedback text-danger" role="alert">
+                            <div class="invalid-feedback d-block">
                                 <strong>{{ $message }}</strong>
-                            </span>
+                            </div>
                         @enderror
-
                     </div>
 
                     <div class="form-group mb-5">
-
                         <label for="username">
                             <i class="fa fa-user custom-color"></i>
                             {{ __('panel.user_name') }}
                             <span class="required">*</span>
                         </label>
-
-                        <input type="text" name="username" id="username" class="form-control  " value=""
-                            placeholder="{{ __('panel.user_name') }}">
+                        <input type="text" name="username" id="username" class="form-control"
+                            value="{{ old('username') }}" placeholder="{{ __('panel.user_name') }}">
 
                         @error('username')
-                            <span class="invalid-feedback text-danger" role="alert">
+                            <div class="invalid-feedback d-block">
                                 <strong>{{ $message }}</strong>
-                            </span>
+                            </div>
                         @enderror
-
                     </div>
 
                     <div class="form-group mb-5">
-
                         <label for="email">
                             <i class="fa fa-envelope custom-color"></i>
                             {{ __('panel.f_user_email') }} <span class="required">*</span>
                         </label>
-
-                        <input type="email" name="email" id="email" class="form-control " value=""
-                            placeholder="your@email.com" autocomplete="email" autocapitalize="none">
+                        <input type="email" name="email" id="email" class="form-control"
+                            value="{{ old('email') }}" placeholder="your@email.com" autocomplete="email"
+                            autocapitalize="none">
 
                         @error('email')
-                            <span class="invalid-feedback text-danger" role="alert">
+                            <div class="invalid-feedback d-block">
                                 <strong>{{ $message }}</strong>
-                            </span>
+                            </div>
                         @enderror
-
                     </div>
 
                     <div class="form-group mb-5">
-
                         <label for="mobile">
                             <i class="fa fa-phone custom-color"></i>
                             {{ __('panel.f_phone_number') }}
                             <span class="required">*</span>
                         </label>
 
+                        <input id="output_mobile" type="hidden" name="full_mobile_number"
+                            value="{{ old('full_mobile_number') }}">
+                        <input id="mobile" type="text" name="mobile" class="form-control"
+                            value="{{ old('mobile') }}">
 
-
-                        <input id="output_mobile" type="hidden" name="full_mobile_number">
-                        <input id="mobile" type="text" name="mobile" class="form-control">
-
-
-
-
-
-
+                        @error('mobile')
+                            <div class="invalid-feedback d-block">
+                                <strong>{{ $message }}</strong>
+                            </div>
+                        @enderror
                     </div>
 
                     <div class="form-group mb-5">
-
                         <label for="password">
                             <i class="fa fa-user custom-color"></i>
                             {{ __('panel.f_user_password') }}
                             <span class="required">*</span>
                         </label>
-
-                        <input type="password" name="password" id="password" class="form-control " value=""
+                        <input type="password" name="password" id="password" class="form-control" value=""
                             placeholder="{{ __('panel.f_password') }}">
 
                         @error('password')
-                            <span class="invalid-feedback text-danger" role="alert">
+                            <div class="invalid-feedback d-block">
                                 <strong>{{ $message }}</strong>
-                            </span>
+                            </div>
                         @enderror
-
                     </div>
 
                     <div class="form-group mb-5">
@@ -152,18 +140,15 @@
                             {{ __('panel.f_confirm_password') }}
                             <span class="required">*</span>
                         </label>
-
-                        <input type="password" name="password_confirmation" id="password_confirmation" class="form-control "
+                        <input type="password" name="password_confirmation" id="password_confirmation" class="form-control"
                             value="" placeholder="{{ __('panel.f_confirm_password') }}">
 
                         @error('password_confirmation')
-                            <span class="invalid-feedback text-danger" role="alert">
+                            <div class="invalid-feedback d-block">
                                 <strong>{{ $message }}</strong>
-                            </span>
+                            </div>
                         @enderror
-
                     </div>
-
 
                     <div class="text-center">
                         <button class="btn btn-block btn-primary" type="submit">

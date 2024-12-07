dcca986619fe3b927ca9e2303d1de810a61ca926
khaleel raweh <khaleelvisa@gmail.com>
2024-12-07 21:08:56 +0300

    save all

--- public/frontend/assets/plugins/telephoneinput/inttelephoneinput.js
+++ public/frontend/assets/plugins/telephoneinput/inttelephoneinput.js
@@ -3,7 +3,8 @@ $(function() {
 	// International Telephone Input
 	var input = document.querySelector("#phone");
     window.intlTelInput(input, {
-      utilsScript: "assets/plugins/telephoneinput/utils.js",
+      utilsScript: "../frontend/assets/plugins/telephoneinput/utils.js",
+      // utilsScript: "assets/plugins/telephoneinput/utils.js",
     });
 });
 

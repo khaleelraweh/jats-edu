c4b35d5020aa638b1408b31b3436111b14abc8c4
khaleel raweh <khaleelvisa@gmail.com>
2024-12-09 18:53:46 +0300

    save all

--- resources/views/frontend/customer/teach_requests/create.blade.php
+++ resources/views/frontend/customer/teach_requests/create.blade.php
@@ -4,8 +4,6 @@
 
 @section('style')
     <link href="{{ URL::asset('frontend/assets/plugins/sidebar/sidebar.css') }}" rel="stylesheet">
-    {{-- <link href="{{ URL::asset('frontend/assets/css-rtl/style.css') }}" rel="stylesheet"> --}}
-    <link href="{{ URL::asset('frontend/assets/css/style' . $rtl . '.css') }}" rel="stylesheet">
     <link href="{{ URL::asset('frontend/assets/plugins/fileuploads/css/fileupload.css') }}" rel="stylesheet" type="text/css" />
 
 

0735914acc2167923211824c5c525bd603fc491e
khaleel raweh <khaleelvisa@gmail.com>
2024-12-09 18:26:11 +0300

    save all

--- resources/views/frontend/customer/teach_requests/create.blade.php
+++ resources/views/frontend/customer/teach_requests/create.blade.php
@@ -1,40 +1,11 @@
 @extends('layouts.app')
+<?php $rtl = config('locales.languages')[app()->getLocale()]['rtl_support'] == 'rtl' ? '-rtl' : ''; ?>
+
 
 @section('style')
-    {{-- This is for master page  --}}
-    <!--  Custom Scroll bar-->
-    <link href="{{ URL::asset('frontend/assets/plugins/mscrollbar/jquery.mCustomScrollbar.css') }}" rel="stylesheet" />
-    <!--  Sidebar css -->
     <link href="{{ URL::asset('frontend/assets/plugins/sidebar/sidebar.css') }}" rel="stylesheet">
-    <!-- Sidemenu css -->
-    <link rel="stylesheet" href="{{ URL::asset('frontend/assets/css-rtl/sidemenu.css') }}">
-    @yield('css')
-    <!--- Style css -->
     <link href="{{ URL::asset('frontend/assets/css-rtl/style.css') }}" rel="stylesheet">
-    <!--- Dark-mode css -->
-    <link href="{{ URL::asset('frontend/assets/css-rtl/style-dark.css') }}" rel="stylesheet">
-    <!---Skinmodes css-->
-    <link href="{{ URL::asset('frontend/assets/css-rtl/skin-modes.css') }}" rel="stylesheet">
-
-    {{-- This is for this page  --}}
-    <link href="{{ URL::asset('frontend/assets/plugins/select2/css/select2.min.css') }}" rel="stylesheet">
-
-
-
-    {{-- start for image upload --}}
-    <!--- Internal Select2 css-->
-    <link href="{{ URL::asset('frontend/assets/plugins/select2/css/select2.min.css') }}" rel="stylesheet">
-    <!---Internal Fileupload css-->
-    <link href="{{ URL::asset('frontend/assets/plugins/fileuploads/css/fileupload.css') }}" rel="stylesheet"
-        type="text/css" />
-    <!---Internal Fancy uploader css-->
-    <link href="{{ URL::asset('frontend/assets/plugins/fancyuploder/fancy_fileupload.css') }}" rel="stylesheet" />
-    <!--Internal Sumoselect css-->
-    <link rel="stylesheet" href="{{ URL::asset('frontend/assets/plugins/sumoselect/sumoselect-rtl.css') }}">
-    <!--Internal  TelephoneInput css-->
-    <link rel="stylesheet" href="{{ URL::asset('frontend/assets/plugins/telephoneinput/telephoneinput-rtl.css') }}">
-
-    {{-- end for image upload --}}
+    <link href="{{ URL::asset('frontend/assets/plugins/fileuploads/css/fileupload.css') }}" rel="stylesheet" type="text/css" />
 
 
     <style>
@@ -68,7 +39,7 @@
 
         <div class="row">
             <div class="col-lg-12 col-md-12">
-                <div class="card">
+                <div class="card" style="text-align: {{ app()->getLocale() == 'ar' ? 'right' : 'left' }};">
                     <div class="card-body">
                         <div class="main-content-label mg-b-5">
                             {{ __('panel.application_form_to_apply_as_a_trainer') }}
@@ -373,41 +344,10 @@
 @endsection
 
 @section('script')
-    {{-- start for image upload --}}
-
-    <!--Internal  Datepicker js -->
-    <script src="{{ URL::asset('frontend/assets/plugins/jquery-ui/ui/widgets/datepicker.js') }}"></script>
-    <!-- Internal Select2 js-->
-    <script src="{{ URL::asset('frontend/assets/plugins/select2/js/select2.min.js') }}"></script>
-    <!--Internal Fileuploads js-->
     <script src="{{ URL::asset('frontend/assets/plugins/fileuploads/js/fileupload.js') }}"></script>
     <script src="{{ URL::asset('frontend/assets/plugins/fileuploads/js/file-upload.js') }}"></script>
-    <!--Internal Fancy uploader js-->
-    <script src="{{ URL::asset('frontend/assets/plugins/fancyuploder/jquery.ui.widget.js') }}"></script>
-    <script src="{{ URL::asset('frontend/assets/plugins/fancyuploder/jquery.fileupload.js') }}"></script>
-    <script src="{{ URL::asset('frontend/assets/plugins/fancyuploder/jquery.iframe-transport.js') }}"></script>
-    <script src="{{ URL::asset('frontend/assets/plugins/fancyuploder/jquery.fancy-fileupload.js') }}"></script>
-    <script src="{{ URL::asset('frontend/assets/plugins/fancyuploder/fancy-uploader.js') }}"></script>
-    <!--Internal  Form-elements js-->
-    <script src="{{ URL::asset('frontend/assets/js/advanced-form-elements.js') }}"></script>
-    <script src="{{ URL::asset('frontend/assets/js/select2.js') }}"></script>
-    <!--Internal Sumoselect js-->
-    <script src="{{ URL::asset('frontend/assets/plugins/sumoselect/jquery.sumoselect.js') }}"></script>
-    <!-- Internal TelephoneInput js-->
-    <script src="{{ URL::asset('frontend/assets/plugins/telephoneinput/telephoneinput.js') }}"></script>
-    <script src="{{ URL::asset('frontend/assets/plugins/telephoneinput/inttelephoneinput.js') }}"></script>
-
-
-    {{-- end for image upload --}}
-
-    <!--Internal  Select2 js -->
-    <script src="{{ URL::asset('frontend/assets/plugins/select2/js/select2.min.js') }}"></script>
-    <!-- Internal Jquery.steps js -->
-    <script src="{{ URL::asset('frontend/assets/plugins/jquery-steps/jquery.steps.min.js') }}"></script>
-    <script src="{{ URL::asset('frontend/assets/plugins/parsleyjs/parsley.min.js') }}"></script>
-    <!--Internal  Form-wizard js -->
+    <script src="{{ URL::asset('frontend/assets/plugins/jquery-steps/jquery.steps' . $rtl . '.min.js') }}"></script>
     <script src="{{ URL::asset('frontend/assets/js/form-wizard.js') }}"></script>
-
 
     <script>
         $(function() {

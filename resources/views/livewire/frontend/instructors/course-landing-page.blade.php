da5cd8381040feac9f32223ca732afcd496329e2
khaleel raweh <khaleelvisa@gmail.com>
2024-12-10 18:10:24 +0300

    save all

--- resources/views/livewire/frontend/instructors/course-landing-page.blade.php
+++ resources/views/livewire/frontend/instructors/course-landing-page.blade.php
@@ -437,7 +437,63 @@
                     content: 'Test 2'
                 }
             ],
-            content_css: []
+            content_css: [],
+
+            // Enable image title and upload functionality
+            image_title: true,
+            automatic_uploads: true,
+            file_picker_types: 'image',
+            file_picker_callback: function(cb, value, meta) {
+                var input = document.createElement('input');
+                input.setAttribute('type', 'file');
+                input.setAttribute('accept', 'image/*');
+
+                input.onchange = function() {
+                    var file = this.files[0];
+                    var reader = new FileReader();
+                    reader.onload = function() {
+                        var id = 'blobid' + (new Date()).getTime();
+                        var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+                        var base64 = reader.result.split(',')[1];
+                        var blobInfo = blobCache.create(id, file, base64);
+                        blobCache.add(blobInfo);
+
+                        // Call the callback and populate the Title field with the file name
+                        cb(blobInfo.blobUri(), {
+                            title: file.name
+                        });
+                    };
+                    reader.readAsDataURL(file);
+                };
+
+                input.click();
+            },
+
+            // Add alignment options for images in the toolbar
+            image_advtab: true,
+            // toolbar2: 'alignleft aligncenter alignright | imageoptions',
+
+            // Add alignment to the context menu
+            contextmenu: 'image align | link',
+
+            // Custom content style for image alignment
+            content_style: `
+                body { font-family:Helvetica,Arial,sans-serif; font-size:14px }
+                img { display: block; margin-left: auto; margin-right: auto; }
+            `,
+
+            // Customize the image dialog to include alignment
+            setup: function(editor) {
+                editor.ui.registry.addContextToolbar('imagealign', {
+                    predicate: function(node) {
+                        return node.nodeName.toLowerCase() === 'img';
+                    },
+                    items: 'alignleft aligncenter alignright',
+                    position: 'node',
+                    scope: 'node'
+                });
+            }
+
         });
     }
 </script>

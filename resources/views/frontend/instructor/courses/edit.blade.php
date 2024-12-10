c9eb9b38b90df9539d1586e619efd2dde0309dca
khaleel raweh <khaleelvisa@gmail.com>
2024-12-10 17:35:32 +0300

    save all

--- resources/views/frontend/instructor/courses/edit.blade.php
+++ resources/views/frontend/instructor/courses/edit.blade.php
@@ -7,8 +7,6 @@
     {{-- <link href="{{ asset('frontend/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" /> --}}
     <!-- Icons Css -->
     <link href="{{ asset('frontend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-    <!-- App Css-->
-    <link href="{{ asset('frontend/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 
 
     {{-- new links  --}}

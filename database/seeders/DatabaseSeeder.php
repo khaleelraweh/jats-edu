04307ecff5b2292352f36392f50043202caca09c
khaleel raweh <khaleelvisa@gmail.com>
2024-12-15 21:23:18 +0300

    save all

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -77,7 +77,7 @@ class DatabaseSeeder extends Seeder
         $this->call(StudentEvaluationSeeder::class);
         $this->call(StudentAnswerSeeder::class);
 
-        // $this->call(CompanyRequestSeeder::class);
+        $this->call(CompanyRequestSeeder::class);
         $this->call(CertificateIssueSeeder::class);
         $this->call(InstPageVisitSeeder::class);
     }

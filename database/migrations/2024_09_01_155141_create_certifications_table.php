c3341796221c4917f81bc3fe95dc28384fdb48b8
khaleel raweh <khaleelvisa@gmail.com>
2024-12-16 21:26:54 +0300

    save all

--- database/migrations/2024_09_01_155141_create_certifications_table.php
+++ database/migrations/2024_09_01_155141_create_certifications_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->dateTime('date_of_issue')->nullable();
             $table->integer('cert_code')->nullable();
 
-            $table->foreignId('sponser_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('sponser_id')->nullable()->constrained()->cascadeOnDelete();
 
 
             $table->timestamps();

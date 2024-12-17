4d63c1a5e9ce5abdfe64988e5606a4637e0da3b9
khaleel raweh <khaleelvisa@gmail.com>
2024-12-17 18:18:01 +0300

    save all

--- app/Http/Controllers/Backend/CertificateController.php
+++ app/Http/Controllers/Backend/CertificateController.php
@@ -79,8 +79,8 @@ class CertificateController extends Controller
 
         if ($image =  $request->file('cert_file')) {
 
-            $file_name                  = Str::slug($request->cert_code) . '_' . time() .  "." . $image->getClientOriginalExtension();
-            $path                       = public_path('assets/certifications/' . $file_name);
+            $file_name                      = Str::slug($request->cert_code) . '_' . time() .  "." . $image->getClientOriginalExtension();
+            $path                           = public_path('assets/certifications/' . $file_name);
 
             Image::make($image->getRealPath())->resize(300, null, function ($constraint) {
                 $constraint->aspectRatio();
@@ -125,28 +125,52 @@ class CertificateController extends Controller
         $certificate = Certifications::where('id', $certificate)->first();
         $courses = Course::query()->active()->get(['id', 'title']);
 
-
-
         return view('backend.certificates.edit', compact('certificate', 'courses'));
     }
 
-    public function update(PageRequest $request, $page)
+    public function update(CertificateRequest $request, $certificate)
     {
 
-        $page = Page::where('id', $page)->first();
+        $certificate = Certifications::where('id', $certificate)->first();
 
-        $input['title'] = $request->title;
-        $input['content'] = $request->content;
 
-        $input['status']            =   $request->status;
-        $input['created_by'] = auth()->user()->full_name;
-        $published_on = $request->published_on . ' ' . $request->published_on_time;
-        $published_on = new DateTimeImmutable($published_on);
-        $input['published_on'] = $published_on;
+        $input['user_id']                   = $request->user_id;
+        $input['full_name']                 = $request->full_name;
+        $input['course_id']                 = $request->course_id;
 
-        $page->update($input);
+        $date_of_issue                      = str_replace(['ص', 'م'], ['AM', 'PM'], $request->date_of_issue);
+        $DateOfIssue                        = Carbon::createFromFormat('Y/m/d h:i A', $date_of_issue)->format('Y-m-d H:i:s');
+        $input['date_of_issue']             = $DateOfIssue;
 
-        if ($page) {
+        $input['cert_code']                 = $request->cert_code;
+
+        $published_on                       = str_replace(['ص', 'م'], ['AM', 'PM'], $request->published_on);
+        $publishedOn                        = Carbon::createFromFormat('Y/m/d h:i A', $published_on)->format('Y-m-d H:i:s');
+        $input['published_on']              = $publishedOn;
+
+
+        $input['status']                    = $request->status;
+        $input['created_by']                = auth()->user()->full_name;
+
+
+        if ($image = $request->file('cert_file')) {
+            if ($certificate->cert_file != null && File::exists('assets/certifications/' . $certificate->cert_file)) {
+                unlink('assets/certifications/' . $certificate->cert_file);
+            }
+
+            $file_name = Str::slug($request->cert_code) . '_' . time() .  "." . $image->getClientOriginalExtension();
+
+            $path = public_path('assets/certifications/' . $file_name);
+            Image::make($image->getRealPath())->resize(500, null, function ($constraint) {
+                $constraint->aspectRatio();
+            })->save($path);
+
+            $input['cert_file'] = $file_name;
+        }
+
+        $certificate->update($input);
+
+        if ($certificate) {
             return redirect()->route('admin.certificates.index')->with([
                 'message' => __('panel.updated_successfully'),
                 'alert-type' => 'success'
@@ -160,15 +184,25 @@ class CertificateController extends Controller
     }
 
 
-    public function destroy($page)
+    public function destroy($certificate)
     {
         if (!auth()->user()->ability('admin', 'delete_certificates')) {
             return redirect('admin/index');
         }
 
-        $page = Page::where('id', $page)->first()->delete();
+        $certificate = Certifications::where('id', $certificate)->first();
 
-        if ($page) {
+
+        if (File::exists('assets/certifications/' . $certificate->cert_file)) {
+            unlink('assets/certifications/' . $certificate->cert_file);
+        }
+
+        $certificate->deleted_by = auth()->user()->full_name;
+        $certificate->save();
+
+        $certificate->delete();
+
+        if ($certificate) {
             return redirect()->route('admin.certificates.index')->with([
                 'message' => __('panel.deleted_successfully'),
                 'alert-type' => 'success'

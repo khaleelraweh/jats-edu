b91cdffdc870b46e95dde5b74e4a6ca05f83c7fb
khaleelraweh <khaleelvisa@gmail.com>
2025-03-21 01:50:34 +0300

    save all

--- app/Http/Livewire/Frontend/Events/EnrollFreeEventComponent.php
+++ app/Http/Livewire/Frontend/Events/EnrollFreeEventComponent.php
@@ -2,12 +2,71 @@
 
 namespace App\Http\Livewire\Frontend\Events;
 
+use App\Models\Course;
+use App\Models\CourseOrder;
+use App\Models\Order;
+use App\Models\OrderTransaction;
 use Livewire\Component;
+use Illuminate\Support\Str;
 
 class EnrollFreeEventComponent extends Component
 {
+
+    public $courseId;
+
+
+    public $payment_method = 3; //mean free
+
     public function render()
     {
         return view('livewire.frontend.events.enroll-free-event-component');
+    }
+
+    public function enrollCourse()
+    {
+        $course = Course::whereId($this->courseId)->firstOrFail();
+        $ref_id = 'ORD-' . Str::random(15);
+
+        //make free order to free course
+        $order = Order::create([
+            'ref_id'                => $ref_id,
+            'user_id'               => auth()->id(),
+            'payment_method_id'     => $this->payment_method,
+            'subtotal'              => 0,
+            'offer_discount'        => 0,
+            'discount_code'         =>  null,
+            'discount'              => getNumbers()->get('discount_coupon'),
+            'shipping'              => getNumbers()->get('shipping'),
+            'tax'                   => getNumbers()->get('courseTaxes'),
+            'total'                 => 0,
+            'currency'              => 'USD',
+            'order_status'          => 0
+        ]);
+
+        //register it to course order
+        CourseOrder::create([
+            'course_id' => $this->courseId,
+            'order_id' => $order->id,
+            'quantity' => 1
+        ]);
+
+        // make transaction new order for free course
+        $order->transactions()->create(
+            ['transaction' => OrderTransaction::NEW_ORDER]
+        );
+
+
+        // update  order to payment completed == change it to free
+        $order->update(['order_status' => Order::Free]);
+
+        // update order transaction to completed == change it to free
+        $order->transactions()->create([
+            'transaction' => OrderTransaction::Free,
+            'transaction_number' => $ref_id,
+            'payment_result' => 'success',
+        ]);
+
+        // redirect student to his enrolled coursed to see his new course
+        redirect()->route("customer.courses");
     }
 }
--- resources/views/livewire/frontend/events/enroll-free-event-component.blade.php
+++ resources/views/livewire/frontend/events/enroll-free-event-component.blade.php
@@ -1,3 +1,35 @@
 <div>
-    {{-- Success is as dangerous as failure. --}}
+
+
+
+
+    @php
+        $userId = auth()->id();
+
+        $course = App\Models\Course::whereId($courseId)->firstOrFail();
+
+        // Check if the user is already enrolled in the course
+        $isEnrolled = App\Models\Order::where('user_id', $userId)
+            ->whereHas('courses', function ($query) use ($courseId) {
+                $query->where('course_id', $courseId);
+            })
+            ->exists();
+
+    @endphp
+
+    @if ($course->isInstructor($userId))
+
+        <a href="{{ route('instructor.courses.edit', $course->id) }}"
+            class="btn btn-blue btn-block mb-6">{{ __('transf.btn_go_to_your_course_dashboard') }}</a>
+    @else
+        @if ($isEnrolled)
+            <a href="{{ route('customer.lesson_single', $course->slug) }}"
+                class="btn btn-blue btn-block mb-6">{{ __('transf.btn_go_to_course') }}</a>
+        @else
+            <button wire:click.prevent="enrollCourse()" class="btn btn-orange btn-block mb-6" type="button"
+                name="button">{{ __('transf.btn_enroll') }}</button>
+        @endif
+    @endif
+
+
 </div>

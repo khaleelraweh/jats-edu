33f634d295cd4bc46ffd3709c8fdf9b8d1431232
khaleelraweh <khaleelvisa@gmail.com>
2025-04-10 18:23:17 +0300

    save all

--- resources/views/partial/frontend/footer.blade.php
+++ resources/views/partial/frontend/footer.blade.php
@@ -350,32 +350,24 @@
                 <div
                     class="border-top pb-5 pt-6 py-md-4 text-center text-xl-start d-flex flex-column d-md-block d-xl-flex flex-xl-row align-items-center">
                     <p
-                        class="text-gray-800 font-size-sm-alone d-block mb-0 mb-md-2 mb-xl-0 order-1 order-md-0 px-9 px-md-0">
+                        class="text-gray-800 font-size-sm-alone d-block mb-0 mb-md-2 mb-xl-0 order-1 order-md-0 px-9 px-md-0 ">
                         {{ __('transf.txt_copyright') }} © {{ now()->year }} {{ $siteSettings['site_name']->value ?? '' }} ,
-                        {{ __('transf.txt_all_right_reserved') }}</p>
+                        {{ __('transf.txt_all_right_reserved') }}
+                    </p>
+
 
                     <div
                         class="ms-xl-auto d-flex flex-column flex-md-row align-items-stretch align-items-md-center justify-content-center">
                         <ul
                             class="navbar-nav flex-row flex-wrap font-size-sm-alone mb-3 mb-md-0 mx-n4 me-md-5 justify-content-center justify-content-lg-start order-1 order-md-0">
-                            <li class="nav-item py-2 py-md-0 px-0 border-top-0">
-                                <a href="index.html"
-                                    class="nav-link px-4 fw-normal text-gray-800">{{ __('transf.lnk_home') }}</a>
-                            </li>
-                            <li class="nav-item py-2 py-md-0 px-0 border-top-0">
-                                <a href="terms-of-service.html"
-                                    class="nav-link px-4 fw-normal text-gray-800">{{ __('transf.lnk_site_map') }}</a>
-                            </li>
-                            <li class="nav-item py-2 py-md-0 px-0 border-top-0">
-                                <a href="terms-of-service.html" class="nav-link px-4 fw-normal text-gray-800">
-                                    {{ __('transf.lnk_privacy_policy') }}</a>
-                            </li>
-                            <li class=" d-none d-xl-flex nav-item py-2 py-md-0 px-0 border-top-0">
-                                <a href="terms-of-service.html" class="nav-link px-4 fw-normal text-gray-800">
-                                    {{ __('transf.lnk_web_use_policy') }}
-                                </a>
-                            </li>
 
+                            @forelse ($web_menus->where('section',6) as $menu_item)
+                                <li class="nav-item py-2 py-md-0 px-0 border-top-0">
+                                    <a href="{{ $menu_item->link != null ? url($menu_item->link) : '#' }}"
+                                        class="nav-link px-4 fw-normal text-gray-800">{{ $menu_item->title }}</a>
+                                </li>
+                            @empty
+                            @endforelse
                         </ul>
 
                         <div class="dropdown me-2">

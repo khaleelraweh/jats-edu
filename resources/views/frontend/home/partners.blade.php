9d322a4edfdd508d38b53155b7d1b0a2faa2bc6f
khaleelraweh <khaleelvisa@gmail.com>
2025-04-10 16:29:05 +0300

    save all

--- resources/views/frontend/home/partners.blade.php
+++ resources/views/frontend/home/partners.blade.php
@@ -8,7 +8,7 @@
         <div class="mx-n3 mx-md-n4"
             data-flickity='{"pageDots": false,"cellAlign": "left", "wrapAround": true, "imagesLoaded": true , "autoPlay": true}'>
             @foreach ($partners as $partner)
-                <div class="col-12 col-md-2 px-1">
+                <div class="col-12 col-md-2 px-1  ">
                     @php
                         if ($partner->partner_image != null) {
                             $partner_img = asset('assets/partners/' . $partner->partner_image);
@@ -21,7 +21,7 @@
                         }
                     @endphp
                     {{-- <div class="flick-item" style="width:190px !important;height: 190px !important;"> --}}
-                    <div class="flick-item">
+                        <div class="flick-item m-4 m-sm-1 ">
                         <a href="{{ $partner->partner_link }}">
                             <img class="partner-img" src="{{ $partner_img }}"
                                 style="width:100%;height:100%;border-radius:50%;" class="img-fluid"

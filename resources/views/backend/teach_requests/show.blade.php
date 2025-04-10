53649344455d1b745aa901aa32a4cad53271dabf
khaleelraweh <khaleelvisa@gmail.com>
2025-04-10 10:21:53 +0300

    save all

--- resources/views/backend/teach_requests/show.blade.php
+++ resources/views/backend/teach_requests/show.blade.php
@@ -142,14 +142,14 @@
                                         للتدريب واللغات</h3>
                                 </div>
                                 <div class="col-sm-4 d-flex align-items-center justify-content-center">
-                                    <img style="width: 250px; padding-top: 10px; z-index: 4"
+                                    <img style="width: 11em; padding-top: 10px; z-index: 4"
                                         src="{{ asset('backend/images/logo.jpg') }}" alt="" />
                                 </div>
                                 <div class="col-sm-4 d-flex flex-column align-items-center justify-content-center">
                                     <h1 class="text-success m-0" style="
-        font-family: Times New Roman, sans-serif; font-size:1.43em; font-weight:800">
+        font-family: Times New Roman, sans-serif; font-size:1em; font-weight:800">
                                         Youth Step Institute</h1>
-                                    <h3 class="text-warning" style=" font-family: Times New Roman, sans-serif;font-size:1em;font-weight:700;margin-top:0.25em">For Training
+                                    <h3 class="text-warning" style=" font-family: Times New Roman, sans-serif;font-size:0.6em;font-weight:700;margin-top:0.25em">For Training
                                         &
                                         Languages
                                     </h3>

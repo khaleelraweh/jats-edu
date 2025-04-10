c314522763b686dc574f22b7fb01653d13a2a181
khaleelraweh <khaleelvisa@gmail.com>
2025-04-10 15:07:43 +0300

    save all

--- resources/views/frontend/home/blog.blade.php
+++ resources/views/frontend/home/blog.blade.php
@@ -8,7 +8,7 @@
         <div class="row align-items-end mb-4 mb-md-7">
             <div class="col-md mb-4 mb-md-0">
             </div>
-            <div class="col-md-auto" data-aos="fade-start">
+            {{-- <div class="col-md-auto" data-aos="fade-start">
                 <a href="{{ route('frontend.blog_list') }}" class="d-flex align-items-center fw-medium">
                     {{ __('transf.browse_all') }}
                     <div class="ms-2 d-flex">
@@ -22,7 +22,77 @@
 
                     </div>
                 </a>
+            </div> --}}
+            <style>
+                .browse-all-link {
+                    position: relative;
+                    display: inline-flex;
+                    align-items: center;
+                    padding-right: 20px; /* Make space for the animation */
+                }
+
+                .arrow-icon {
+                    position: relative;
+                    transition: transform 0.4s ease;
+                }
+
+                .browse-all-link:hover .arrow-icon {
+                    animation: slideRight 0.6s ease;
+                }
+
+                @keyframes slideRight {
+                    0% {
+                        transform: translateX(0);
+                    }
+                    100% {
+                        transform: translateX(100%);
+                    }
+                }
+
+                .browse-all-link {
+                    position: relative;
+                    display: inline-flex;
+                    align-items: center;
+                    padding-right: 20px;
+                }
+
+                .arrow-icon {
+                    position: relative;
+                }
+
+                .browse-all-link:hover .arrow-icon {
+                    animation: slideRight 1s infinite ease-in-out;
+                }
+
+                @keyframes slideRight {
+                    0% {
+                        transform: translateX(0);
+                        opacity: 0;
+                    }
+                    50% {
+                        opacity: 1;
+                    }
+                    100% {
+                        transform: translateX(100%);
+                        opacity: 0;
+                    }
+                }
+            </style>
+            <div class="col-md-auto" data-aos="fade-start">
+                <a href="{{ route('frontend.blog_list') }}" class="d-flex align-items-center fw-medium browse-all-link">
+                    {{ __('transf.browse_all') }}
+                    <div class="ms-2 arrow-icon">
+                        <!-- Icon -->
+                        <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M7.7779 4.6098L3.32777 0.159755C3.22485 0.0567475 3.08745 0 2.94095 0C2.79445 0 2.65705 0.0567475 2.55412 0.159755L2.2264 0.487394C2.01315 0.700889 2.01315 1.04788 2.2264 1.26105L5.96328 4.99793L2.22225 8.73895C2.11933 8.84196 2.0625 8.97928 2.0625 9.1257C2.0625 9.27228 2.11933 9.4096 2.22225 9.51269L2.54998 9.84025C2.65298 9.94325 2.7903 10 2.9368 10C3.0833 10 3.2207 9.94325 3.32363 9.84025L7.7779 5.38614C7.88107 5.2828 7.93774 5.14484 7.93741 4.99817C7.93774 4.85094 7.88107 4.71305 7.7779 4.6098Z"
+                                fill="currentColor" />
+                        </svg>
+                    </div>
+                </a>
             </div>
+
+
         </div>
 
         <div class="row row-cols-md-2 row-cols-lg-3">
